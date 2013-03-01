436a4cb271f9abbde4d7c4740335a6be5323ebcf
Kenneth Chan <kenneth@tappingstone.com>
2013-03-01 21:16:40 +0800

    update data requirement description

--- commons/src/main/scala/io/prediction/commons/settings/code/CodeAlgoInfos.scala
+++ commons/src/main/scala/io/prediction/commons/settings/code/CodeAlgoInfos.scala
@@ -61,7 +61,7 @@ class CodeAlgoInfos extends AlgoInfos {
         "conflictParam"),
       enginetype = "itemrec",
       techreq = Seq("Hadoop"),
-      datareq = Seq("U2I Actions such as Like, Buy and Rate.")
+      datareq = Seq("Users, Items, and U2I Actions such as Like, Buy and Rate.")
     ),
     "mahout-itembased" -> AlgoInfo(
       id = "mahout-itembased",
@@ -95,7 +95,7 @@ class CodeAlgoInfos extends AlgoInfos {
         "conflictParam"),
       enginetype = "itemrec",
       techreq = Seq("Hadoop"),
-      datareq = Seq("U2I Actions such as Like, Buy and Rate.")
+      datareq = Seq("Users, Items, and U2I Actions such as Like, Buy and Rate.")
     ),
     "mahout-parallelals" -> AlgoInfo(
       id = "mahout-parallelals",
@@ -141,7 +141,7 @@ class CodeAlgoInfos extends AlgoInfos {
         "conflictParam"),
       enginetype = "itemrec",
       techreq = Seq("Hadoop"),
-      datareq = Seq("U2I Actions such as Like, Buy and Rate.")
+      datareq = Seq("Users, Items, and U2I Actions such as Like, Buy and Rate.")
     ),
     "pdio-randomrank" -> AlgoInfo(
       id = "pdio-randomrank",
