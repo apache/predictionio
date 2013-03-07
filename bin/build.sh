ab5746428a3be9e601680408fd95d985d3ebcfe8
Donald Szeto <donald@tappingstone.com>
2013-03-07 16:39:28 +0800

    PDIO-45 #comment Add Play detection and auto install

--- bin/build.sh
+++ bin/build.sh
@@ -1,39 +1,93 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # PredictionIO Build Script
 
+# Utilities
+command_exists () {
+	command -v "$1" >/dev/null 2>&1
+}
+
+install_play () {
+	VENDORS_PATH=$1
+	mkdir -p $VENDORS_PATH
+	cd $VENDORS_PATH
+	curl -o play-2.1.0.zip http://downloads.typesafe.com/play/2.1.0/play-2.1.0.zip
+	unzip play-2.1.0.zip
+}
+
+# Get the absolute path of the build script
+SCRIPT="$0"
+while [ -h "$SCRIPT" ] ; do
+	SCRIPT=`readlink "$SCRIPT"`
+done
+
 # Get the base directory of the repo
-cd `dirname $0`/..
-base=`pwd`
+DIR=`dirname $SCRIPT`/..
+cd $DIR
+BASE=`pwd`
+VENDORS_PATH="$BASE/vendors"
+
+# Detect existing installations in search path
+if command_exists "sbt" ; then
+	echo "Using sbt in search path. No additional JVM optimization will be set."
+	SBT=sbt
+else
+	echo >&2 "Unable to locate sbt. Aborting."
+	exit 1
+fi
+
+if command_exists "play" ; then
+	PLAY=play
+else
+	if [ -x $VENDORS_PATH/play-2.1.0/play ] ; then
+		echo "Using play in vendors."
+		PLAY=$VENDORS_PATH/play-2.1.0/play
+	elif install_play "$VENDORS_PATH" ; then
+		echo "Going to download and install Play Framework 2.1.0..."
+		PLAY=$VENDORS_PATH/play-2.1.0/play
+	else
+		echo >&2 "Unable to locate play and automatic installation failed. Aborting."
+		exit 1
+	fi
+fi
+
+# Full rebuild?
+if test "$REBUILD" = "1" ; then
+	echo "Rebuild set."
+	CLEAN=clean
+else
+	echo "Incremental build set."
+	CLEAN=
+fi
 
 # Build commons
-cd $base/commons
-sbt clean update +publish
+cd $BASE/commons
+$SBT $CLEAN update +publish
 
 # Build output
-cd $base/output
-sbt clean update +publish
+cd $BASE/output
+$SBT $CLEAN update +publish
 
 # Build process assembly
-cd $base/process/hadoop/scala
-sbt clean update assembly
+cd $BASE/process/hadoop/scala
+$SBT $CLEAN update assembly
 
 # Build MAP@k Top-k Items Collector
-cd $base/process/hadoop/scala/engines/itemrec/evaluations/topkitems
-sbt clean update assembly
+cd $BASE/process/hadoop/scala/engines/itemrec/evaluations/topkitems
+$SBT $CLEAN update assembly
 
 # Build user tool
-cd $base/tools/users
-sbt clean update pack
+cd $BASE/tools/users
+$SBT $CLEAN update pack
 
 # Build admin server
-cd $base/adminServer
-play clean update compile
+cd $BASE/adminServer
+$PLAY $CLEAN update compile
 
 # Build API server
-cd $base/output/api
-play clean update compile
+cd $BASE/output/api
+$PLAY $CLEAN update compile
 
 # Build scheduler server
-cd $base/scheduler
-play clean update compile
+cd $BASE/scheduler
+$PLAY $CLEAN update compile
