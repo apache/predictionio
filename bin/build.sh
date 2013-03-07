806324a3ca1a201241d6fa3c92507afcd71fadc9
Donald Szeto <donald@tappingstone.com>
2013-03-07 17:19:19 +0800

    PDIO-45 PDIO-77 #comment Add sbt detection and auto install

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,18 @@ command_exists () {
 	command -v "$1" >/dev/null 2>&1
 }
 
+install_sbt () {
+	echo "Going to download and install sbt 0.12.2..."
+	VENDORS_PATH=$1/sbt-0.12.2
+	mkdir -p $VENDORS_PATH
+	cd $VENDORS_PATH
+	curl -o sbt-launch.jar http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.12.2/sbt-launch.jar
+	echo 'java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=512M -jar `dirname $0`/sbt-launch.jar "$@"' > sbt
+	chmod a+x sbt
+}
+
 install_play () {
+	echo "Going to download and install Play Framework 2.1.0..."
 	VENDORS_PATH=$1
 	mkdir -p $VENDORS_PATH
 	cd $VENDORS_PATH
@@ -25,30 +36,36 @@ done
 DIR=`dirname $SCRIPT`/..
 cd $DIR
 BASE=`pwd`
+
+# Third party software
 VENDORS_PATH="$BASE/vendors"
+VENDOR_SBT="$VENDORS_PATH/sbt-0.12.2/sbt"
+VENDOR_PLAY="$VENDORS_PATH/play-2.1.0/play"
 
 # Detect existing installations in search path
 if command_exists "sbt" ; then
 	echo "Using sbt in search path. No additional JVM optimization will be set."
 	SBT=sbt
+elif [ -x "$VENDOR_SBT" ] ; then
+	echo "Using sbt in vendors."
+	SBT="$VENDOR_SBT"
+elif install_sbt "$VENDORS_PATH" ; then
+	SBT="$VENDOR_SBT"
 else
-	echo >&2 "Unable to locate sbt. Aborting."
+	echo "Unable to locate sbt and automatic installation failed. Aborting." >&2
 	exit 1
 fi
 
 if command_exists "play" ; then
 	PLAY=play
+elif [ -x "$VENDOR_PLAY" ] ; then
+	echo "Using play in vendors."
+	PLAY="$VENDOR_PLAY"
+elif install_play "$VENDORS_PATH" ; then
+	PLAY="$VENDOR_PLAY"
 else
-	if [ -x $VENDORS_PATH/play-2.1.0/play ] ; then
-		echo "Using play in vendors."
-		PLAY=$VENDORS_PATH/play-2.1.0/play
-	elif install_play "$VENDORS_PATH" ; then
-		echo "Going to download and install Play Framework 2.1.0..."
-		PLAY=$VENDORS_PATH/play-2.1.0/play
-	else
-		echo >&2 "Unable to locate play and automatic installation failed. Aborting."
-		exit 1
-	fi
+	echo "Unable to locate play and automatic installation failed. Aborting." >&2
+	exit 1
 fi
 
 # Full rebuild?
