2e8db63d93cdc57612181a6edd1b7022c9740a64
Donald Szeto <donald@tappingstone.com>
2013-03-11 18:12:25 +0800

    PDIO-45 #comment Automatic installation of Mahout

--- dist/bin/setup-vendors.sh
+++ dist/bin/setup-vendors.sh
@@ -35,15 +35,28 @@ else
 fi
 
 # Detect existing installations in search path
-#if command_exists "hadoop" ; then
-#	echo "Found hadoop in search path. Assuming Apache Hadoop has been installed."
-if vendor_hadoop_exists ; then
+if command_exists "hadoop" ; then
+	echo "Found hadoop in search path. Assuming Apache Hadoop has been installed."
+elif vendor_hadoop_exists ; then
 	echo "Found hadoop in vendors area. Assuming Apache Hadoop has been installed."
 else
 	while true; do
 		read -p "Cannot find hadoop from search path nor vendors area. Do you want to automatically install $VENDOR_HADOOP_NAME? (Please make sure you can SSH to the localhost without a password.) [y/n] " yn
 		case $yn in
 			[Yy]* ) install_hadoop "$VENDORS_PATH"; break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer 'y' or 'n'.";;
+		esac
+	done
+fi
+
+if vendor_mahout_exists ; then
+	echo "Found $VENDOR_MAHOUT_NAME in vendors area. Assuming $VENDOR_MAHOUT_NAME has been installed."
+else
+	while true; do
+		read -p "Cannot find $VENDOR_MAHOUT_NAME from vendors area. Do you want to automatically install $VENDOR_MAHOUT_NAME? [y/n] " yn
+		case $yn in
+			[Yy]* ) install_mahout "$VENDORS_PATH"; break;;
 			[Nn]* ) break;;
 			* ) echo "Please answer 'y' or 'n'.";;
 		esac
