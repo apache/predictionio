09afa2cafaa5c64171f0d141624d1a68d7f8307b
Donald Szeto <donald@tappingstone.com>
2013-03-12 22:59:17 +0800

    PDIO-80 #comment Clean up

--- build.sbt
+++
@@ -1,7 +0,0 @@
-name := "PredictionIO"
-
-version := "0.1"
-
-organization := "io.prediction"
-
-scalaVersion := "2.9.2"
--- project/Build.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-import Keys._
-
-object PredictionIOBuild extends Build {
-  lazy val root = Project(id = "predictionio", base = file(".")) aggregate(commons)
-
-  lazy val commons = Project(id = "predictionio-commons", base = file("commons"))
-}
