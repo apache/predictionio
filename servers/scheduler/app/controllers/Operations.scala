538c3296b3f7f0598027a98bbeb53d3e5d414a67
Donald Szeto <donald@tappingstone.com>
2013-03-19 23:13:49 +0800

    PDIO-54 #comment Generic HDFS operations via hadoop shell commands

--- servers/scheduler/app/controllers/Operations.scala
+++ servers/scheduler/app/controllers/Operations.scala
@@ -3,8 +3,7 @@ package io.prediction.scheduler
 import io.prediction.commons.filepath.BaseDir
 import io.prediction.commons.Config
 
-//import org.apache.hadoop.conf.Configuration
-//import org.apache.hadoop.fs.{FileSystem, Path}
+import scala.sys.process._
 
 import play.api._
 import play.api.libs.json._
@@ -13,61 +12,48 @@ import play.api.mvc._
 object Operations extends Controller {
   val config = new Config
 
-  /** Hadoop stuff. */
-  //val conf = new Configuration
-  //val fileSystem = FileSystem.get(conf)
-
   def deleteApp(appid: Int) = Action {
-    //val path = new Path(BaseDir.appDir(config.settingsHdfsRoot, appid))
     val path = BaseDir.appDir(config.settingsHdfsRoot, appid)
-    try {
-      //fileSystem.delete(path, true)
-      Ok(Json.obj("message" -> "Deleted HDFS storage for App ID: %d (%s)".format(appid, path)))
-    } catch {
-      case e: java.io.IOException => InternalServerError(Json.obj("message" -> "Unable to delete HDFS storage for App ID: %d (%s) due to an exception: %s".format(appid, path, e.getMessage)))
-    }
+    val code = s"${Scheduler.hadoopCommand} fs -rmr $path".!
+    if (code == 0)
+      Ok(Json.obj("message" -> s"Deleted HDFS storage for App ID: $appid ($path)"))
+    else
+      InternalServerError(Json.obj("message" -> s"Unable to delete HDFS storage for App ID: $appid ($path)"))
   }
 
   def deleteEngine(appid: Int, engineid: Int) = Action {
-    //val path = new Path(BaseDir.engineDir(config.settingsHdfsRoot, appid, engineid))
     val path = BaseDir.engineDir(config.settingsHdfsRoot, appid, engineid)
-    try {
-      //fileSystem.delete(path, true)
-      Ok(Json.obj("message" -> "Deleted HDFS storage for App ID: %d, Engine ID: %d (%s)".format(appid, engineid, path)))
-    } catch {
-      case e: java.io.IOException => InternalServerError(Json.obj("message" -> "Unable to delete HDFS storage for App ID: %d, Engine ID: %d (%s) due to an exception: %s".format(appid, engineid, path, e.getMessage)))
-    }
+    val code = s"${Scheduler.hadoopCommand} fs -rmr $path".!
+    if (code == 0)
+      Ok(Json.obj("message" -> s"Deleted HDFS storage for App ID: $appid, Engine ID: $engineid ($path)"))
+    else
+      InternalServerError(Json.obj("message" -> s"Unable to delete HDFS storage for App ID: $appid, Engine ID: $engineid ($path)"))
   }
 
   def deleteAlgo(appid: Int, engineid: Int, algoid: Int) = Action {
-    //val path = new Path(BaseDir.algoDir(config.settingsHdfsRoot, appid, engineid, algoid, None))
     val path = BaseDir.algoDir(config.settingsHdfsRoot, appid, engineid, algoid, None)
     deleteAlgoBase(path, appid, engineid, algoid, None)
   }
 
   def deleteOfflineEval(appid: Int, engineid: Int, offlineevalid: Int) = Action {
-    //val path = new Path(BaseDir.offlineEvalDir(config.settingsHdfsRoot, appid, engineid, offlineevalid))
     val path = BaseDir.offlineEvalDir(config.settingsHdfsRoot, appid, engineid, offlineevalid)
-    try {
-      //fileSystem.delete(path, true)
-      Ok(Json.obj("message" -> "Deleted HDFS storage for App ID: %d, Engine ID: %d, OfflineEval ID: %d (%s)".format(appid, engineid, offlineevalid, path)))
-    } catch {
-      case e: java.io.IOException => InternalServerError(Json.obj("message" -> "Unable to delete HDFS storage for App ID: %d, Engine ID: %d, OfflineEval ID: %d (%s) due to an exception: %s".format(appid, engineid, offlineevalid, path, e.getMessage)))
-    }
+    val code = s"${Scheduler.hadoopCommand} fs -rmr $path".!
+    if (code == 0)
+      Ok(Json.obj("message" -> s"Deleted HDFS storage for App ID: $appid, Engine ID: $engineid, OfflineEval ID: $offlineevalid ($path)"))
+    else
+      InternalServerError(Json.obj("message" -> s"Unable to delete HDFS storage for App ID: $appid, Engine ID: $engineid, OfflineEval ID: $offlineevalid ($path)"))
   }
 
   def deleteOfflineEvalAlgo(appid: Int, engineid: Int, offlineevalid: Int, algoid: Int) = Action {
-    //val path = new Path(BaseDir.algoDir(config.settingsHdfsRoot, appid, engineid, algoid, Some(offlineevalid)))
     val path = BaseDir.algoDir(config.settingsHdfsRoot, appid, engineid, algoid, Some(offlineevalid))
     deleteAlgoBase(path, appid, engineid, algoid, Some(offlineevalid))
   }
 
   def deleteAlgoBase(path: String, appid: Int, engineid: Int, algoid: Int, offlineevalid: Option[Int]) = {
-    try {
-      //fileSystem.delete(path, true)
-      Ok(Json.obj("message" -> "Deleted HDFS storage for App ID: %d, Engine ID: %d, Algo ID: %d, OfflineEval ID: %s (%s)".format(appid, engineid, algoid, offlineevalid.getOrElse("N/A"), path)))
-    } catch {
-      case e: java.io.IOException => InternalServerError(Json.obj("message" -> "Unable to delete HDFS storage for App ID: %d, Engine ID: %d, Algo ID: %d, OfflineEval ID: %s (%s) due to an exception: %s".format(appid, engineid, algoid, offlineevalid.getOrElse("N/A"), path, e.getMessage)))
-    }
+    val code = s"${Scheduler.hadoopCommand} fs -rmr $path".!
+    if (code == 0)
+      Ok(Json.obj("message" -> s"Deleted HDFS storage for App ID: $appid, Engine ID: $engineid, Algo ID: $algoid, OfflineEval ID: ${offlineevalid.getOrElse("N/A")} ($path)"))
+    else
+      InternalServerError(Json.obj("message" -> s"Unable to delete HDFS storage for App ID: $appid, Engine ID: $engineid, Algo ID: $algoid, OfflineEval ID: ${offlineevalid.getOrElse("N/A")} ($path)"))
   }
 }
