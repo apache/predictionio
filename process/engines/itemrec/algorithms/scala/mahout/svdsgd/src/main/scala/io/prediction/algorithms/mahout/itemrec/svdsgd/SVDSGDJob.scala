3c3abc5bb3d5fa5cd8abe7fa65a38883c320bc3d
Kenneth Chan <kenneth@tappingstone.com>
2013-03-27 14:44:22 +0800

    PDIO-74 #comment add SVDSGD and SVDPlusPlus job

--- process/engines/itemrec/algorithms/scala/mahout/svdsgd/src/main/scala/io/prediction/algorithms/mahout/itemrec/svdsgd/SVDSGDJob.scala
+++ process/engines/itemrec/algorithms/scala/mahout/svdsgd/src/main/scala/io/prediction/algorithms/mahout/itemrec/svdsgd/SVDSGDJob.scala
@@ -1,4 +1,4 @@
-package io.prediction.algorithms.mahout.itemrec.alswr
+package io.prediction.algorithms.mahout.itemrec.svdsgd
 
 import scala.collection.JavaConversions._
 
@@ -7,23 +7,26 @@ import io.prediction.commons.mahout.itemrec.MahoutJob
 import org.apache.mahout.cf.taste.model.DataModel
 import org.apache.mahout.cf.taste.recommender.Recommender
 import org.apache.mahout.cf.taste.impl.recommender.svd.SVDRecommender
-import org.apache.mahout.cf.taste.impl.recommender.svd.ALSWRFactorizer
+import org.apache.mahout.cf.taste.impl.recommender.svd.RatingSGDFactorizer
 import org.apache.mahout.cf.taste.impl.recommender.svd.Factorizer
 
-class ALSWRJob extends MahoutJob {
+class SVDSGDJob extends MahoutJob {
 
   override def buildRecommender(dataModel: DataModel, args: Map[String, String]): Recommender = {
 
     val numFeatures: Int = getArg(args, "numFeatures").toInt
-    val lambda: Double = getArg(args, "lambda").toDouble
+    val learningRate: Double = getArg(args, "learningRate").toDouble
+    val preventOverfitting: Double = getArg(args, "preventOverfitting").toDouble
+    val randomNoise: Double = getArg(args, "randomNoise").toDouble
     val numIterations: Int = getArg(args, "numIterations").toInt
+    val learningRateDecay: Double = getArg(args, "learningRateDecay").toDouble
 
-    val factorizer: Factorizer = new ALSWRFactorizer(dataModel, numFeatures, lambda, numIterations)
-
-    // default stratagy is PreferredItemsNeighborhoodCandidateItemsStrategy();
+    val factorizer: Factorizer = new RatingSGDFactorizer(dataModel, numFeatures, learningRate, preventOverfitting,
+      randomNoise, numIterations, learningRateDecay)
+    
     val recommender: Recommender = new SVDRecommender(dataModel, factorizer)
 
     recommender
   }
-
 }
+
