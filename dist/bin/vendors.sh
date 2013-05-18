6d44925c40a4f5b7cab5b6f621c205a65616430d
Donald Szeto <donald@tappingstone.com>
2013-05-19 00:45:10 +0800

    PDIO-176 Bump to Hadoop 1.1.2 and MongoDB 2.4.3

--- dist/bin/vendors.sh
+++ dist/bin/vendors.sh
@@ -4,13 +4,13 @@
 
 # Third party software
 VENDORS_PATH="$BASE/vendors"
-VENDOR_HADOOP_PATH="$VENDORS_PATH/hadoop-1.0.4"
+VENDOR_HADOOP_PATH="$VENDORS_PATH/hadoop-1.1.2"
 VENDOR_MAHOUT_PATH="$VENDORS_PATH/mahout-0.8-snapshot"
-VENDOR_MONGODB_PATH="$VENDORS_PATH/mongodb-linux-x86_64-2.4.1"
+VENDOR_MONGODB_PATH="$VENDORS_PATH/mongodb-linux-x86_64-2.4.3"
 
-VENDOR_HADOOP_NAME="Apache Hadoop 1.0.4"
+VENDOR_HADOOP_NAME="Apache Hadoop 1.1.2"
 VENDOR_MAHOUT_NAME="Apache Mahout 0.8-SNAPSHOT Build 1993"
-VENDOR_MONGODB_NAME="MongoDB 2.4.1 (64-bit Linux)"
+VENDOR_MONGODB_NAME="MongoDB 2.4.3 (64-bit Linux)"
 
 # Utilities
 command_exists () {
@@ -26,8 +26,8 @@ install_mongodb () {
 	local VENDORS_PATH=$1
 	mkdir -p $VENDORS_PATH
 	cd $VENDORS_PATH
-	curl -o mongodb-linux-x86_64-2.4.1.tgz http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.4.1.tgz
-	tar zxvf mongodb-linux-x86_64-2.4.1.tgz
+	curl -o mongodb-linux-x86_64-2.4.3.tgz http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.4.3.tgz
+	tar zxvf mongodb-linux-x86_64-2.4.3.tgz
 }
 
 install_hadoop () {
@@ -45,11 +45,11 @@ install_hadoop () {
 	mkdir -p $VENDORS_PATH
 	cd $VENDORS_PATH
 	echo "Retrieving Apache mirror list..."
-	curl -o apache_hadoop_mirrors.txt http://www.apache.org/dyn/closer.cgi/hadoop/common/hadoop-1.0.4/hadoop-1.0.4.tar.gz
+	curl -o apache_hadoop_mirrors.txt http://www.apache.org/dyn/closer.cgi/hadoop/common/hadoop-1.1.2/hadoop-1.1.2.tar.gz
 	HADOOP_URL=$(cat apache_hadoop_mirrors.txt | grep -m 1 "<strong>.*</strong>" | sed 's/.*<strong>//' | sed 's/<\/strong>.*//')
 	echo "Found mirror: $HADOOP_URL"
-	curl -o hadoop-1.0.4.tar.gz $HADOOP_URL
-	tar zxvf hadoop-1.0.4.tar.gz
+	curl -o hadoop-1.1.2.tar.gz $HADOOP_URL
+	tar zxvf hadoop-1.1.2.tar.gz
 	echo "Configuring Hadoop in pseudo-distributed mode..."
 	cp ../conf/hadoop/* $VENDOR_HADOOP_PATH/conf
 	echo "export JAVA_HOME=$JAVA_HOME" >> $VENDOR_HADOOP_PATH/conf/hadoop-env.sh
