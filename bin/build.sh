07e5ed3303ec032d38e219bfc977df895fed3b78
Donald Szeto <donald@tappingstone.com>
2013-05-24 01:09:59 +0800

    Add shortcut to bypass building process assemblies

--- bin/build.sh
+++ bin/build.sh
@@ -44,39 +44,43 @@ echo "Going to build PredictionIO Process Commons..."
 cd $BASE/process/commons/hadoop/scalding
 $SBT $CLEAN update +publish
 
-# Build process itemrec algo assembly
-echo "Going to build PredictionIO Process ItemRec Hadoop Scalding Algorithms Assembly..."
-cd $BASE/process/engines/itemrec/algorithms/hadoop/scalding
-$SBT $CLEAN update assembly
-
-echo "Going to build PredictionIO Process ItemRec Scala Mahout Algorithms Assembly..."
-cd $BASE/process/engines/itemrec/algorithms/scala/mahout
-$SBT $CLEAN update assembly
-
-# Build process itemrec eval assembly
-echo "Going to build PredictionIO Process ItemRec Evaluations Assembly..."
-cd $BASE/process/engines/itemrec/evaluations/hadoop/scalding
-$SBT $CLEAN update assembly
-
-# Build process itemrec parameter generator
-echo "Going to build PredictionIO Parameter Generator Assembly..."
-cd $BASE/process/engines/itemrec/evaluations/scala/paramgen
-$SBT $CLEAN update assembly
-
-# Build process itemrec training test split assembly
-echo "Going to build PredictionIO Training-Test Split Assembly..."
-cd $BASE/process/engines/itemrec/evaluations/scala/trainingtestsplit
-$SBT $CLEAN update assembly
-
-# Build process itemrec Top-k Items Collector
-echo "Going to build PredictionIO Top-k Items Collector Assembly..."
-cd $BASE/process/engines/itemrec/evaluations/scala/topkitems
-$SBT $CLEAN update assembly
-
-# Build process itemsim algo assembly
-echo "Going to build PredictionIO Process ItemRec Algorithms Assembly..."
-cd $BASE/process/engines/itemsim/algorithms/hadoop/scalding
-$SBT $CLEAN update assembly
+if test "$SKIP_PROCESS" = "1" ; then
+    echo "Skip building process assemblies."
+else
+    # Build process itemrec algo assembly
+    echo "Going to build PredictionIO Process ItemRec Hadoop Scalding Algorithms Assembly..."
+    cd $BASE/process/engines/itemrec/algorithms/hadoop/scalding
+    $SBT $CLEAN update assembly
+    
+    echo "Going to build PredictionIO Process ItemRec Scala Mahout Algorithms Assembly..."
+    cd $BASE/process/engines/itemrec/algorithms/scala/mahout
+    $SBT $CLEAN update assembly
+    
+    # Build process itemrec eval assembly
+    echo "Going to build PredictionIO Process ItemRec Evaluations Assembly..."
+    cd $BASE/process/engines/itemrec/evaluations/hadoop/scalding
+    $SBT $CLEAN update assembly
+    
+    # Build process itemrec parameter generator
+    echo "Going to build PredictionIO Parameter Generator Assembly..."
+    cd $BASE/process/engines/itemrec/evaluations/scala/paramgen
+    $SBT $CLEAN update assembly
+    
+    # Build process itemrec training test split assembly
+    echo "Going to build PredictionIO Training-Test Split Assembly..."
+    cd $BASE/process/engines/itemrec/evaluations/scala/trainingtestsplit
+    $SBT $CLEAN update assembly
+    
+    # Build process itemrec Top-k Items Collector
+    echo "Going to build PredictionIO Top-k Items Collector Assembly..."
+    cd $BASE/process/engines/itemrec/evaluations/scala/topkitems
+    $SBT $CLEAN update assembly
+    
+    # Build process itemsim algo assembly
+    echo "Going to build PredictionIO Process ItemRec Algorithms Assembly..."
+    cd $BASE/process/engines/itemsim/algorithms/hadoop/scalding
+    $SBT $CLEAN update assembly
+fi
 
 # Build connection check tool
 echo "Going to build PredictionIO Connection Check Tool..."
