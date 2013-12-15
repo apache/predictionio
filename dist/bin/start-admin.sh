5e97c5dedfcf954e30bc4089374c98f48c129a35
Donald Szeto <donald@prediction.io>
2013-12-15 22:38:43 +0800

    Add individual server startup support

--- dist/bin/start-admin.sh
+++ dist/bin/start-admin.sh
@@ -2,10 +2,12 @@
 
 # PredictionIO Admin Server Startup Script
 
+set -e
+
 # Get the absolute path of the build script
 SCRIPT="$0"
 while [ -h "$SCRIPT" ] ; do
-	SCRIPT=`readlink "$SCRIPT"`
+    SCRIPT=`readlink "$SCRIPT"`
 done
 
 # Get the base directory of the repo
@@ -14,68 +16,28 @@ cd $DIR
 BASE=`pwd`
 
 . "$BASE/bin/common.sh"
+. "$BASE/bin/vendors.sh"
 
-LIB_DIR="$BASE/lib"
+mkdir -p "$LOGDIR"
 
-JARS="${JARS}:$LIB_DIR/ch.qos.logback.logback-classic-1.0.13.jar"
-JARS="${JARS}:$LIB_DIR/ch.qos.logback.logback-core-1.0.13.jar"
-JARS="${JARS}:$LIB_DIR/com.esotericsoftware.kryo.kryo-2.21.jar"
-JARS="${JARS}:$LIB_DIR/com.esotericsoftware.minlog.minlog-1.2.jar"
-JARS="${JARS}:$LIB_DIR/com.esotericsoftware.reflectasm.reflectasm-1.07.jar"
-JARS="${JARS}:$LIB_DIR/com.fasterxml.jackson.core.jackson-annotations-2.2.2.jar"
-JARS="${JARS}:$LIB_DIR/com.fasterxml.jackson.core.jackson-core-2.2.2.jar"
-JARS="${JARS}:$LIB_DIR/com.fasterxml.jackson.core.jackson-databind-2.2.2.jar"
-JARS="${JARS}:$LIB_DIR/com.github.nscala-time.nscala-time_2.10-0.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.github.scala-incubator.io.scala-io-core_2.10-0.4.2.jar"
-JARS="${JARS}:$LIB_DIR/com.github.scala-incubator.io.scala-io-file_2.10-0.4.2.jar"
-JARS="${JARS}:$LIB_DIR/com.jsuereth.scala-arm_2.10-1.3.jar"
-JARS="${JARS}:$LIB_DIR/com.ning.async-http-client-1.7.18.jar"
-JARS="${JARS}:$LIB_DIR/com.twitter.bijection-core_2.10-0.4.0.jar"
-JARS="${JARS}:$LIB_DIR/com.twitter.chill_2.10-0.2.3.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.akka.akka-actor_2.10-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.akka.akka-slf4j_2.10-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.config-1.0.2.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.netty.netty-http-pipelining-1.1.2.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.play.play-datacommons_2.10-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.play.play-exceptions-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.play.play-functional_2.10-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.play.play-iteratees_2.10-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.play.play-json_2.10-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.play.play_2.10-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.play.sbt-link-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/com.typesafe.play.templates_2.10-2.2.0.jar"
-JARS="${JARS}:$LIB_DIR/commons-codec.commons-codec-1.8.jar"
-JARS="${JARS}:$LIB_DIR/commons-logging.commons-logging-1.1.1.jar"
-JARS="${JARS}:$LIB_DIR/io.netty.netty-3.7.0.Final.jar"
-JARS="${JARS}:$LIB_DIR/io.prediction.predictionio-admin-${VERSION}.jar"
-JARS="${JARS}:$LIB_DIR/io.prediction.predictionio-commons_2.10-${VERSION}.jar"
-JARS="${JARS}:$LIB_DIR/io.prediction.predictionio-output_2.10-${VERSION}.jar"
-JARS="${JARS}:$LIB_DIR/javax.transaction.jta-1.1.jar"
-JARS="${JARS}:$LIB_DIR/joda-time.joda-time-2.2.jar"
-JARS="${JARS}:$LIB_DIR/oauth.signpost.signpost-commonshttp4-1.2.1.2.jar"
-JARS="${JARS}:$LIB_DIR/oauth.signpost.signpost-core-1.2.1.2.jar"
-JARS="${JARS}:$LIB_DIR/org.apache.commons.commons-lang3-3.1.jar"
-JARS="${JARS}:$LIB_DIR/org.apache.httpcomponents.httpclient-4.0.1.jar"
-JARS="${JARS}:$LIB_DIR/org.apache.httpcomponents.httpcore-4.0.1.jar"
-JARS="${JARS}:$LIB_DIR/org.javassist.javassist-3.18.0-GA.jar"
-JARS="${JARS}:$LIB_DIR/org.joda.joda-convert-1.3.1.jar"
-JARS="${JARS}:$LIB_DIR/org.mongodb.casbah-commons_2.10-2.6.2.jar"
-JARS="${JARS}:$LIB_DIR/org.mongodb.casbah-core_2.10-2.6.2.jar"
-JARS="${JARS}:$LIB_DIR/org.mongodb.casbah-gridfs_2.10-2.6.2.jar"
-JARS="${JARS}:$LIB_DIR/org.mongodb.casbah-query_2.10-2.6.2.jar"
-JARS="${JARS}:$LIB_DIR/org.mongodb.mongo-java-driver-2.11.2.jar"
-JARS="${JARS}:$LIB_DIR/org.objenesis.objenesis-1.2.jar"
-JARS="${JARS}:$LIB_DIR/org.ow2.asm.asm-4.0.jar"
-JARS="${JARS}:$LIB_DIR/org.ow2.asm.asm-commons-4.0.jar"
-JARS="${JARS}:$LIB_DIR/org.ow2.asm.asm-tree-4.0.jar"
-JARS="${JARS}:$LIB_DIR/org.scala-lang.scala-library-2.10.2.jar"
-JARS="${JARS}:$LIB_DIR/org.scala-lang.scala-reflect-2.10.2.jar"
-JARS="${JARS}:$LIB_DIR/org.scala-stm.scala-stm_2.10-0.7.jar"
-JARS="${JARS}:$LIB_DIR/org.slf4j.jcl-over-slf4j-1.7.5.jar"
-JARS="${JARS}:$LIB_DIR/org.slf4j.jul-to-slf4j-1.7.5.jar"
-JARS="${JARS}:$LIB_DIR/org.slf4j.slf4j-api-1.7.5.jar"
-JARS="${JARS}:$LIB_DIR/xerces.xercesImpl-2.11.0.jar"
-JARS="${JARS}:$LIB_DIR/xml-apis.xml-apis-1.4.01.jar"
+SERVER_WAIT=1
+SERVER_RETRY=20
 
-mkdir -p $ADMIN_DIR
-exec java $@ -cp "$JARS" play.core.server.NettyServer $ADMIN_DIR
+$BASE/bin/conncheck
+
+# Admin server
+echo "Trying to start admin server... \c"
+echo "Trying to start admin server at: `date`" >>"$ADMIN_OUT"
+$BASE/bin/predictionio-admin $PLAY_START_OPTS -Dhttp.port=$ADMIN_PORT -Dlogger.file=$BASE/conf/admin-logger.xml -Dpidfile.path=$BASE/admin.pid >>"$ADMIN_OUT" 2>>"$ADMIN_ERR" &
+SERVER_TRY=1
+while [ $SERVER_TRY -le $SERVER_RETRY ] ; do
+    sleep $SERVER_WAIT
+    if [ $(curl --write-out %{http_code} --silent --output /dev/null "localhost:$ADMIN_PORT") -eq 303 ] ; then
+        echo "started"
+        SERVER_TRY=$SERVER_RETRY
+    elif [ $SERVER_TRY -eq $SERVER_RETRY ] ; then
+        echo "failed ($ADMIN_PORT unreachable)"
+        exit 1
+    fi
+    SERVER_TRY=$((SERVER_TRY+1))
+done
--- dist/bin/start-all.sh
+++ dist/bin/start-all.sh
@@ -7,7 +7,7 @@ set -e
 # Get the absolute path of the build script
 SCRIPT="$0"
 while [ -h "$SCRIPT" ] ; do
-	SCRIPT=`readlink "$SCRIPT"`
+    SCRIPT=`readlink "$SCRIPT"`
 done
 
 # Get the base directory of the repo
@@ -25,77 +25,30 @@ SERVER_RETRY=20
 
 # MongoDB
 if vendor_mongodb_exists ; then
-	while true; do
-		read -p "Found MongoDB in vendors area. Do you want to start it? [y/n] " yn
-		case $yn in
-			[Yy]* ) start_mongodb; break;;
-			[Nn]* ) break;;
-			* ) echo "Please answer 'y' or 'n'.";;
-		esac
-	done
+    while true; do
+        read -p "Found MongoDB in vendors area. Do you want to start it? [y/n] " yn
+        case $yn in
+            [Yy]* ) start_mongodb; break;;
+            [Nn]* ) break;;
+            * ) echo "Please answer 'y' or 'n'.";;
+        esac
+    done
 fi
 
 $BASE/bin/conncheck
 
-# Admin server
-echo "Trying to start admin server... \c"
-echo "Trying to start admin server at: `date`" >>"$ADMIN_OUT"
-$BASE/bin/predictionio-admin $PLAY_START_OPTS -Dhttp.port=$ADMIN_PORT -Dlogger.file=$BASE/conf/admin-logger.xml -Dpidfile.path=$BASE/admin.pid >>"$ADMIN_OUT" 2>>"$ADMIN_ERR" &
-SERVER_TRY=1
-while [ $SERVER_TRY -le $SERVER_RETRY ] ; do
-	sleep $SERVER_WAIT
-	if [ $(curl --write-out %{http_code} --silent --output /dev/null "localhost:$ADMIN_PORT") -eq 303 ] ; then
-	    echo "started"
-	    SERVER_TRY=$SERVER_RETRY
-	elif [ $SERVER_TRY -eq $SERVER_RETRY ] ; then
-	    echo "failed ($ADMIN_PORT unreachable)"
-	    exit 1
-	fi
-	SERVER_TRY=$((SERVER_TRY+1))
-done
-
-# API server
-echo "Trying to start API server... \c"
-echo "Trying to start API server at: `date`" >>"$API_OUT"
-$BASE/bin/predictionio-api $PLAY_START_OPTS -Dhttp.port=$API_PORT -Dlogger.file=$BASE/conf/api-logger.xml -Dpidfile.path=$BASE/api.pid >>"$API_OUT" 2>>"$API_ERR" &
-SERVER_TRY=1
-while [ $SERVER_TRY -le $SERVER_RETRY ] ; do
-	sleep $SERVER_WAIT
-	if [ $(curl --write-out %{http_code} --silent --output /dev/null "localhost:$API_PORT") -eq 200 ] ; then
-	    echo "started"
-	    SERVER_TRY=$SERVER_RETRY
-	elif [ $SERVER_TRY -eq $SERVER_RETRY ] ; then
-	    echo "failed ($API_PORT unreachable)"
-	    exit 1
-	fi
-	SERVER_TRY=$((SERVER_TRY+1))
-done
-
-# Scheduler server
-echo "Trying to start scheduler server... \c"
-echo "Trying to start scheduler server at: `date`" >>"$SCHEDULER_OUT"
-$BASE/bin/predictionio-scheduler $PLAY_START_OPTS -Dhttp.port=$SCHEDULER_PORT -Dlogger.file=$BASE/conf/scheduler-logger.xml -Dpidfile.path=$BASE/scheduler.pid >>"$SCHEDULER_OUT" 2>>"$SCHEDULER_ERR" &
-SERVER_TRY=1
-while [ $SERVER_TRY -le $SERVER_RETRY ] ; do
-	sleep $SERVER_WAIT
-	if [ $(curl --write-out %{http_code} --silent --output /dev/null "localhost:$SCHEDULER_PORT") -eq 200 ] ; then
-	    echo "started"
-	    SERVER_TRY=$SERVER_RETRY
-	elif [ $SERVER_TRY -eq $SERVER_RETRY ] ; then
-	    echo "failed ($SCHEDULER_PORT unreachable)"
-	    exit 1
-	fi
-	SERVER_TRY=$((SERVER_TRY+1))
-done
+$BASE/bin/start-admin.sh
+$BASE/bin/start-api.sh
+$BASE/bin/start-scheduler.sh
 
 # Apache Hadoop
 if vendor_hadoop_exists ; then
-	while true; do
-		read -p "Found Hadoop in vendors area. Do you want to start it? [y/n] " yn
-		case $yn in
-			[Yy]* ) start_hadoop; break;;
-			[Nn]* ) break;;
-			* ) echo "Please answer 'y' or 'n'.";;
-		esac
-	done
+    while true; do
+        read -p "Found Hadoop in vendors area. Do you want to start it? [y/n] " yn
+        case $yn in
+            [Yy]* ) start_hadoop; break;;
+            [Nn]* ) break;;
+            * ) echo "Please answer 'y' or 'n'.";;
+        esac
+    done
 fi
