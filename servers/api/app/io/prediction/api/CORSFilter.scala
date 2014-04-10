f84f6dd6b4204e53aaa1e7d0b317cf529c227563
Qui Nguyen <qui@mit.edu>
2014-04-10 15:53:58 -0400

    Allow user to configure allowed domain for CORS

--- servers/api/app/io/prediction/api/CORSFilter.scala
+++ servers/api/app/io/prediction/api/CORSFilter.scala
@@ -4,6 +4,7 @@ import play.api.mvc.{ SimpleResult, RequestHeader, Filter }
 case class CORSFilter() extends Filter {
   import scala.concurrent._
   import ExecutionContext.Implicits.global
+  lazy val allowedDomain = play.api.Play.current.configuration.getString("cors.allowed.domain")
 
   def isPreflight(r: RequestHeader) = {
     r.method.toLowerCase.equals("options") && r.headers.get("Access-Control-Request-Method").nonEmpty
@@ -12,12 +13,12 @@ case class CORSFilter() extends Filter {
   def apply(f: (RequestHeader) => Future[SimpleResult])(request: RequestHeader): Future[SimpleResult] = {
     if (isPreflight(request)) {
       Future.successful(Default.Ok.withHeaders(
-        "Access-Control-Allow-Origin" -> "*",
+        "Access-Control-Allow-Origin" -> allowedDomain.getOrElse(""),
         "Access-Control-Allow-Methods" -> "GET, POST, DELETE"
       ))
     } else {
       f(request).map(_.withHeaders(
-        "Access-Control-Allow-Origin" -> "*"
+        "Access-Control-Allow-Origin" -> allowedDomain.getOrElse("")
       ))
     }
   }
