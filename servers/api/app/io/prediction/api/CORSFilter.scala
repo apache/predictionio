52396d9003b4b9d44592051071afc376b2ccb169
Qui Nguyen <qui@mit.edu>
2014-04-11 01:15:51 -0400

    Allow user to set list of allowed domains for CORS (rather than just one)

--- servers/api/app/io/prediction/api/CORSFilter.scala
+++ servers/api/app/io/prediction/api/CORSFilter.scala
@@ -4,21 +4,34 @@ import play.api.mvc.{ SimpleResult, RequestHeader, Filter }
 case class CORSFilter() extends Filter {
   import scala.concurrent._
   import ExecutionContext.Implicits.global
-  lazy val allowedDomain = play.api.Play.current.configuration.getString("cors.allowed.domain")
+  lazy val allowedDomains = play.api.Play.current.configuration.getString("cors.allowed.domains").getOrElse("").split(",")
 
   def isPreflight(r: RequestHeader) = {
     r.method.toLowerCase.equals("options") && r.headers.get("Access-Control-Request-Method").nonEmpty
   }
 
+  /**
+  * If the origin of the request is allowed, return the origin.
+  * Otherwise, return "" (no CORS access allowed).
+  */
+  def getOriginAllowed(requestOrigin: String): String = {
+    for (allowedOrig <- allowedDomains) {
+      if (allowedDomain == "*" || requestOrigin == allowedDomain) {
+        return requestOrigin
+      }
+    }
+    return ""
+  }
+
   def apply(f: (RequestHeader) => Future[SimpleResult])(request: RequestHeader): Future[SimpleResult] = {
     if (isPreflight(request)) {
       Future.successful(Default.Ok.withHeaders(
-        "Access-Control-Allow-Origin" -> allowedDomain.getOrElse(""),
+        "Access-Control-Allow-Origin" -> getOriginAllowed(request.headers.get("Origin").getOrElse("")),
         "Access-Control-Allow-Methods" -> "GET, POST, DELETE"
       ))
     } else {
       f(request).map(_.withHeaders(
-        "Access-Control-Allow-Origin" -> allowedDomain.getOrElse("")
+        "Access-Control-Allow-Origin" -> getOriginAllowed(request.headers.get("Origin").getOrElse(""))
       ))
     }
   }
