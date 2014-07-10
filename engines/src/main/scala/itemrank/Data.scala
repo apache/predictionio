02e58409b0581fe46eaa30f2e0ddc408a24ca70a
Kenneth Chan <kenneth@prediction.io>
2014-07-09 18:58:06 -0700

    Default itypes = None (include all itypes)

--- engines/src/main/scala/itemrank/Data.scala
+++ engines/src/main/scala/itemrank/Data.scala
@@ -7,7 +7,8 @@ import io.prediction.BaseParams
 // param to evaluator, it applies to both DataPrep and Validator
 class EvalParams(
     val appid: Int,
-    val itypes: Option[Set[String]],
+    // default None to include all itypes
+    val itypes: Option[Set[String]] = None,
     // action for training
     val actions: Set[String],
     val hours: Int,
@@ -35,9 +36,7 @@ class TrainDataPrepParams(
     // use all data if None
     val startUntil: Option[Tuple2[DateTime, DateTime]]
   ) extends BaseParams {
-    override def toString = {
-      startUntil.map( x => s"start=${x._1} until=${x._2}").getOrElse("All")
-    }
+    override def toString = s"${appid} ${itypes} ${actions} ${startUntil}"
   }
 
 // param for preparing evaluation data
@@ -48,7 +47,8 @@ class ValidationDataPrepParams(
     val startUntil: Tuple2[DateTime, DateTime],
     val goal: Set[String] // action name
   ) extends BaseParams {
-    override def toString = s"start=${startUntil._1} until=${startUntil._2}"
+    override def toString = s"${appid} ${itypes}" +
+      s" ${actions} ${startUntil} ${goal}"
   }
 
 class CleanserParams (
@@ -56,18 +56,24 @@ class CleanserParams (
   // use None if use u2iActions.v field
   val actions: Map[String, Option[Int]], // ((view, 1), (rate, None))
   val conflict: String // conflict resolution, "latest" "highest" "lowest"
-) extends BaseParams {}
+) extends BaseParams {
+  override def toString = s"${actions} ${conflict}"
+}
 
 class ItemTD(
   val iid: String,
   val itypes: Seq[String],
   val starttime: Option[Long],
   val endtime: Option[Long],
-  val inactive: Boolean) extends Serializable
+  val inactive: Boolean) extends Serializable {
+    override def toString = s"${iid}"
+  }
 
 class UserTD(
   val uid: String
-) extends Serializable
+) extends Serializable {
+  override def toString = s"${uid}"
+}
 
 class U2IActionTD(
   val uindex: Int,
@@ -75,25 +81,33 @@ class U2IActionTD(
   val action: String, // action name
   val v: Option[Int],
   val t: Long // action time
-) extends Serializable
+) extends Serializable {
+  override def toString = s"${uindex} ${iindex} ${action}"
+}
 
 class TrainingData(
     val users: Map[Int, UserTD], // uindex->uid
     val items: Map[Int, ItemTD], // iindex->itemTD
     val u2iActions: Seq[U2IActionTD]
-  ) extends Serializable
+  ) extends Serializable {
+    override def toString = s"TrainingData: ${u2iActions} ${users}"
+  }
 
 class RatingTD(
   val uindex: Int,
   val iindex: Int,
   val rating: Int,
-  val t: Long) extends Serializable
+  val t: Long) extends Serializable {
+    override def toString = s"RatingTD: ${uindex} ${iindex} ${rating}"
+  }
 
 class CleansedData(
   val users: Map[Int, UserTD],
   val items: Map[Int, ItemTD],
   val rating: Seq[RatingTD]
-)
+) extends Serializable {
+  override def toString = s"CleansedData: ${rating}"
+}
 
 // "Feature" here means prediction Input
 class Feature(
