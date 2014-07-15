1ddb8813e40cfbac539ebba861a166eba05ebace
Kenneth Chan <kenneth@prediction.io>
2014-07-15 12:01:57 -0700

    Refactor itemrec MahoutAlgo

--- engines/src/main/java/engines/java/itemrec/algos/GenericItemBased.java
+++ engines/src/main/java/engines/java/itemrec/algos/GenericItemBased.java
@@ -4,7 +4,6 @@ import io.prediction.engines.java.itemrec.data.TrainingData;
 import io.prediction.engines.java.itemrec.data.Feature;
 import io.prediction.engines.java.itemrec.data.Prediction;
 import io.prediction.engines.java.itemrec.data.Model;
-
 import io.prediction.java.JavaLocalAlgorithm;
 
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
@@ -25,14 +24,17 @@ import org.slf4j.LoggerFactory;
 
 // use TrainigData as CD for now
 public class GenericItemBased
-  extends JavaLocalAlgorithm<TrainingData, Feature, Prediction,
-  Model, GenericItemBasedParams> {
+  extends MahoutAlgo<GenericItemBasedParams> {
 
   final static Logger logger = LoggerFactory.getLogger(GenericItemBased.class);
 
   @Override
-  public Model train(TrainingData cd) {
+  public Logger getLogger() {
+    return logger;
+  }
 
+  @Override
+  public Recommender buildRecommender(TrainingData cd) {
     // TODO: support other similarity measure
     ItemSimilarity similarity = new LogLikelihoodSimilarity(cd.dataModel);
 
@@ -45,65 +47,6 @@ public class GenericItemBased
       GenericItemBasedRecommender.getDefaultMostSimilarItemsCandidateItemsStrategy()*/
       );
 
-    Map<Long, List<RecommendedItem>> itemRecScores = new HashMap<Long, List<RecommendedItem>>();
-
-    Iterator<Long> uids;
-    try {
-      uids = cd.dataModel.getUserIDs();
-    } catch (TasteException e){
-      logger.error("Caught TasteException " + e.getMessage());
-      uids = (new ArrayList<Long>()).iterator();
-    }
-
-    // TODO: refactor this with other recommmedner
-    // TOOD: get numRecommendation from params
-    while (uids.hasNext()) {
-      Long uid = uids.next();
-      List<RecommendedItem> items;
-      try {
-        items = recommender.recommend(uid, 100);
-      } catch (TasteException e) {
-        items = new ArrayList<RecommendedItem>();
-        logger.error("Caught TasteException " + e.getMessage());
-      }
-      if (items.size() > 0) {
-        itemRecScores.put(uid, items);
-      }
-    }
-
-    Model m = new Model(itemRecScores);
-    logger.info(m.toString());
-    return m;
-  }
-
-  @Override
-  public Prediction predict(Model model, Feature feature) {
-
-    List<RecommendedItem> items = model.itemRecScores.get((long) feature.uid);
-
-    logger.info(model.itemRecScores.keySet().toString());
-    if (items != null) {
-      logger.info(items.toString());
-    } else {
-      logger.info("null");
-    }
-
-    List<Integer> iids = new ArrayList<Integer>();
-    List<Float> scores = new ArrayList<Float>();
-
-    if (items != null) {
-
-      int i = 0;
-      for (Iterator<RecommendedItem> it = items.iterator();
-        it.hasNext() && (i < feature.n);
-        i++) {
-          RecommendedItem item = it.next();
-          // TODO: check long to int casting
-          iids.add((int) item.getItemID());
-          scores.add(item.getValue());
-        }
-    }
-
-    return new Prediction(iids, scores);
+    return recommender;
   }
 }
