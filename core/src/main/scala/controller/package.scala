75a0b957df7177b0f78758866080f0574280117b
Donald Szeto <donald@prediction.io>
2014-07-24 23:04:29 -0700

    ScalaDoc

---
+++ core/src/main/scala/controller/package.scala
@@ -0,0 +1,5 @@
+package io.prediction
+/** Provides building blocks for writing a complete prediction engine
+  * consisting of DataSource, Preparator, Algorithm, Serving, and Metrics.
+  */
+package object controller {}
