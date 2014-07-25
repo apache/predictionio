1fa5863fd998b311d536744f4838b3503ed6500d
Donald Szeto <donald@prediction.io>
2014-07-24 21:07:31 -0700

    More ScalaDoc

--- core/src/main/scala/controller/Params.scala
+++ core/src/main/scala/controller/Params.scala
@@ -1,10 +1,11 @@
 package io.prediction.controller
 
+/** Base trait for all kinds of parameters that will be passed to constructors
+  * of different controller classes.
+  */
 trait Params extends Serializable {}
 
-// Concrete helper classes
+/** A concrete implementation of [[Params]] representing empty parameters. */
 case class EmptyParams() extends Params {
-  override
-  def toString(): String = "Empty"
+  override def toString(): String = "Empty"
 }
-
