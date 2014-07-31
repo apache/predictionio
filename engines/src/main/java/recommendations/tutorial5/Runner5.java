12548a9ee5d6ff4af62872862ed91897964f03cd
Kenneth Chan <kenneth@prediction.io>
2014-07-30 18:39:03 -0700

    Add one more Mahout example

---
+++ engines/src/main/java/recommendations/tutorial5/Runner5.java
@@ -0,0 +1,44 @@
+package io.prediction.engines.java.recommendations.tutorial5;
+
+import io.prediction.engines.java.recommendations.tutorial1.DataSourceParams;
+import io.prediction.engines.java.recommendations.tutorial3.Metrics;
+
+import io.prediction.controller.EmptyParams;
+import io.prediction.controller.IEngineFactory;
+import io.prediction.controller.java.JavaSimpleEngine;
+import io.prediction.controller.java.JavaSimpleEngineBuilder;
+import io.prediction.controller.java.JavaEngineParams;
+import io.prediction.controller.java.JavaEngineParamsBuilder;
+import io.prediction.workflow.JavaAPIDebugWorkflow;
+
+import java.util.HashMap;
+
+public class Runner5 {
+
+  public static void runEvaluation(String filePath) {
+    JavaEngineParams engineParams = new JavaEngineParamsBuilder()
+      .dataSourceParams(new DataSourceParams(filePath))
+      .addAlgorithmParams("MyMahoutRecommendationAlgo",
+        new MahoutAlgoParams("LogLikelihoodSimilarity"))
+      .build();
+
+    JavaAPIDebugWorkflow.runEngine(
+      "MyEngine",
+      new HashMap<String, String>(),
+      3, // verbose
+      (new EngineFactory()).apply(),
+      engineParams,
+      Metrics.class,
+      new EmptyParams()
+    );
+  }
+
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("Error: Please specify the file path as argument");
+      System.exit(1);
+    }
+    runEvaluation(args[0]);
+    System.exit(0); // clean shutdown is needed for spark
+  }
+}
