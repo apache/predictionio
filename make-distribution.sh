34eb93ed635d26c48a64f7b1e9f792c15fe5b388
Donald Szeto <donald@prediction.io>
2014-08-05 00:44:24 -0700

    Add build step

--- make-distribution.sh
+++ make-distribution.sh
@@ -5,6 +5,9 @@ set -e
 FWDIR="$(cd `dirname $0`; pwd)"
 DISTDIR="$FWDIR/dist"
 
+cd $FWDIR
+sbt/sbt tools/assembly
+
 rm -rf $DISTDIR
 mkdir -p $DISTDIR/bin
 mkdir -p $DISTDIR/conf
@@ -18,8 +21,11 @@ rm $DISTDIR/conf/pio-env.sh
 
 touch $DISTDIR/RELEASE
 
+TARNAME=imagine.tar.gz
 TARDIR=imagine
 cp -r $DISTDIR $TARDIR
 
-tar zcvf imagine.tar.gz $TARDIR
+tar zcvf $TARNAME $TARDIR
 rm -rf $TARDIR
+
+echo "PredictionIO binary distribution created at $TARNAME"
