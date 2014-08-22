77001f37bdf28581c9cd4dd37becd150460f2d92
Donald Szeto <donald@prediction.io>
2014-08-21 19:36:49 -0700

    Remove orphan code

--- tools/src/main/scala/RegisterEngine.scala
+++ tools/src/main/scala/RegisterEngine.scala
@@ -1,45 +1,21 @@
 package io.prediction.tools
 
+import io.prediction.storage.Storage
+import io.prediction.storage.EngineManifest
+import io.prediction.storage.EngineManifestSerializer
+
 import grizzled.slf4j.Logging
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
 import org.json4s._
-import org.json4s.native.Serialization.{ read, write }
+import org.json4s.native.Serialization.read
 
 import scala.io.Source
 
 import java.io.File
 
-import io.prediction.storage.Storage
-import io.prediction.storage.EngineManifest
-import io.prediction.storage.EngineManifestSerializer
-
 object RegisterEngine extends Logging {
-  case class Args(
-    jsonManifest: File = new File("engine.json"),
-    engineFiles: Seq[File] = Seq())
-
-  def main(args: Array[String]): Unit = {
-    val parser = new scopt.OptionParser[Args]("RegisterEngine") {
-      arg[File]("<engine manifest JSON file>") action { (x, c) =>
-        c.copy(jsonManifest = x)
-      } validate { x =>
-        if (x.exists)
-          success
-        else
-          failure(s"${x.getCanonicalPath} was not found.")
-      } text("the JSON file that contains the engine's manifest")
-      arg[File]("<engine files>...") unbounded() action { (x, c) =>
-        c.copy(engineFiles = c.engineFiles :+ x)
-      } text("engine files (JARs)")
-    }
-
-    parser.parse(args, Args()) map { config =>
-      registerEngine(config.jsonManifest, config.engineFiles)
-    }
-  }
-
   def registerEngine(jsonManifest: File, engineFiles: Seq[File]): Unit = {
     implicit val formats = DefaultFormats + new EngineManifestSerializer
     val jsonString = try {
