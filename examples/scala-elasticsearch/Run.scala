53e4a591acb24a120e79c293fc54fe090f622e02
Justin Yip <yipjustin@prediction.io>
2014-08-28 18:37:58 -0700

    reading itemtrends

--- examples/scala-elasticsearch/Run.scala
+++ examples/scala-elasticsearch/Run.scala
@@ -8,8 +8,23 @@ import org.apache.spark.rdd.RDD
 
 import org.apache.hadoop.io.Text
 import org.apache.hadoop.io.MapWritable
+import org.apache.hadoop.io.Writable
 import org.apache.hadoop.conf.Configuration
 import org.elasticsearch.hadoop.mr.EsInputFormat
+
+import _root_.java.util.{ Set => JSet }
+import _root_.java.util.Map.{ Entry => JMapEntry }
+
+import scala.collection.JavaConversions._
+
+import com.google.gson.Gson
+
+object ESItemTrend {
+  def get(mw: MapWritable): String = {
+    mw.entrySet.map { _.getKey.toString }.mkString(" ")
+  }
+}
+
 
 case class DataSource
   extends PDataSource[EmptyParams, AnyRef, AnyRef, AnyRef, AnyRef] {
@@ -18,16 +33,28 @@ case class DataSource
     println("Test")
 
     val conf = new Configuration()
-    conf.set("es.resource", "bank/account")
-    conf.set("es.query", "?q=*")
+    conf.set("es.resource", "predictionio_appdata/itemtrends")
+    conf.set("es.query", "?q=appid:1008&size=1")
 
     val esRDD = sc.newAPIHadoopRDD(
       conf, 
       classOf[EsInputFormat[Text, MapWritable]],
       classOf[Text], 
       classOf[MapWritable])
+      
 
-    esRDD.map(_._2.toString).collect.foreach(println)
+    println("X")
+    //esRDD.map(_._2.toString).collect.foreach(println)
+   
+    println("Y")
+    //esRDD.map(_._2.entrySet.toString).collect.foreach(println)
+    //esRDD.map(e => ESItemTrend.get(e._2.entrySet)).collect.foreach(println)
+    esRDD.map(e => ESItemTrend.get(e._2)).take(10).foreach(println)
+
+    println("Z")
+    esRDD.map(e => e._2.toString).take(20).foreach(println)
+
+    //esRDD.map(e => new Gson().toJson(e._2.entrySet)).take(20).foreach(println)
 
     Seq[(AnyRef, AnyRef, RDD[(AnyRef, AnyRef)])]()
   }
