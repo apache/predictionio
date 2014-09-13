9afc550b72cab8dba4602fb5f33c236823279af1
Kenneth Chan <kenneth@prediction.io>
2014-09-12 17:39:03 -0700

    Add itemrank quickstart

---
+++ sdk/python-sdk/examples/itemrank_quickstart_query.py
@@ -0,0 +1,23 @@
+"""
+itemrank quickstart query
+"""
+
+import predictionio
+
+client = predictionio.PredictionClient()
+
+# Rank item 1 to 5 for each user
+item_ids = [str(i) for i in range(1, 6)]
+user_ids = [str(x) for x in range(1, 6)]
+for user_id in user_ids:
+  print "Rank item 1 to 5 for user", user_id
+  try:
+    response = client.send_query({
+      "uid": user_id,
+      "items": item_ids
+    })
+    print response
+  except predictionio.PredictionIOAPIError as e:
+    print 'Caught exception:', e.strerror()
+
+client.close()
