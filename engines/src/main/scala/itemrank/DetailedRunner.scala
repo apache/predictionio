d86af332f65ee6952adfe9ef1149dc6f9792df64
Justin Yip <yipjustin@prediction.io>
2014-09-15 22:51:27 -0700

    Fix typo.

--- engines/src/main/scala/itemrank/DetailedRunner.scala
+++ engines/src/main/scala/itemrank/DetailedRunner.scala
@@ -11,7 +11,7 @@ object DetailedRunner {
 
   def main(args: Array[String]) {
 
-    val dsp = EventsDataSoureParams(
+    val dsp = EventsDataSourceParams(
       appId = 1,
       itypes = None,
       actions = Set("view", "like", "dislike", "conversion", "rate"),
