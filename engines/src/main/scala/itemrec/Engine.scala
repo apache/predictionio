938d930b4a5a22465a97097ce522c99acd7e643e
Justin Yip <yipjustin@prediction.io>
2014-09-16 23:16:17 -0700

    fix filename.

--- engines/src/main/scala/itemrec/Engine.scala
+++ engines/src/main/scala/itemrec/Engine.scala
@@ -1,5 +1,3 @@
-/*
-
 package io.prediction.engines.itemrec
 
 import io.prediction.controller.IEngineFactory
@@ -7,14 +5,21 @@ import io.prediction.controller.Engine
 
 import io.prediction.engines.itemrank.EventsDataSource
 
-*/
+//import io.prediction.engines.itemrec.NewItemRecDataSource
+//import io.prediction.engines.itemrec.NewItemRecPreparator
+import io.prediction.engines.java.itemrec.algos.GenericItemBased
+import io.prediction.engines.java.itemrec.algos.SVDPlusPlus
+import io.prediction.engines.java.itemrec.ItemRecServing
 
-/*
+
 object ItemRecEngine extends IEngineFactory {
   def apply() = {
     new Engine(
-      classOf[EventsDataSource]
-    )
+      classOf[NewItemRecDataSource],
+      classOf[NewItemRecPreparator],
+      Map(
+        "genericitembased" -> classOf[GenericItemBased],
+        "svdplusplus" -> classOf[SVDPlusPlus]),
+      classOf[ItemRecServing])
   }
 }
-*/
