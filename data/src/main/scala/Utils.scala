3dad8cedbc3776fde654efa052e1e8ca818bbd68
Justin Yip <yipjustin@prediction.io>
2014-09-19 03:05:08 -0700

    Change datetime parser to accept datetime string without millisec.

--- data/src/main/scala/Utils.scala
+++ data/src/main/scala/Utils.scala
@@ -3,13 +3,31 @@ package io.prediction.data
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
+import java.lang.IllegalArgumentException
+
 object Utils {
 
   // use dateTime() for strict ISO8601 format
   val dateTimeFormatter = ISODateTimeFormat.dateTime().withOffsetParsed()
 
-  def stringToDateTime(dt: String): DateTime =
-    dateTimeFormatter.parseDateTime(dt)
+  val dateTimeNoMillisFormatter = 
+    ISODateTimeFormat.dateTimeNoMillis().withOffsetParsed()
+
+  def stringToDateTime(dt: String): DateTime = {
+    // We accept two formats.
+    // 1. "yyyy-MM-dd'T'HH:mm:ss.SSSZZ"
+    // 2. "yyyy-MM-dd'T'HH:mm:ssZZ"
+    // The first one also takes milliseconds into account. 
+    try {
+      // formatting for "yyyy-MM-dd'T'HH:mm:ss.SSSZZ"
+      dateTimeFormatter.parseDateTime(dt)
+    } catch {
+      case e: IllegalArgumentException => {
+        // handle when the datetime string doesn't specify milliseconds.
+        dateTimeNoMillisFormatter.parseDateTime(dt)
+      }
+    }
+  }
 
   def dateTimeToString(dt: DateTime): String = dateTimeFormatter.print(dt)
     //dt.toString
