07a4e57d79354e622b6f821dd156928cb137dba5
yigh <yizhou12345@gmail.com>
2014-10-13 19:30:27 -0500

    added engine files

---
+++ examples/scala-local-friend-recommendation/README.md
@@ -0,0 +1,26 @@
+Engine is based off of Hello World engine template.
+
+
+Register (Build) + Train + Deploy : 
+
+```
+$PIO_HOME/bin/pio register 
+
+$PIO_HOME/bin/pio train 
+
+$PIO_HOME/bin/pio deploy
+```
+
+
+To query :
+
+```
+curl -H "Content-Type: application/json" -d '{ "userId": 888 , "seed" : 999}'
+http://localhost:8000/queries.json
+```
+
+
+UserId is harded coded (because we are currently using synthetic dataset). For
+actual data, we will use userId from dataset.
+Seed is used to seed randomization process. It is optional paramete : the
+default is hardcoded value.
---
+++ examples/scala-local-friend-recommendation/RandomAlgoEngine.scala
@@ -0,0 +1,97 @@
+package org.sample.helloworld
+
+import io.prediction.controller._
+
+import scala.io.Source
+import scala.collection.immutable.HashMap
+
+// For random algorithm
+import scala.util.Random
+
+case class UserItemPair(userId:Int, recommendationId:Int)
+
+// all data need to be serializable
+class MyTrainingData(
+  // list of (day, temperature) tuples
+  val temperatures: List[(String, Double)]
+) extends Serializable
+
+class MyQuery(
+  val userId: Int,
+  val seed: Option[Long]
+) extends Serializable {
+  val randomSeed : Long= seed.getOrElse(5)
+}
+
+class MyModel(
+  val recommendations: Map[Int, List[UserItemPair]]
+) extends Serializable {
+  override def toString = recommendations.toString
+}
+
+class MyPrediction(
+  val recommendationList: List[UserItemPair]
+) extends Serializable
+
+// controller components
+class MyDataSource extends LDataSource[EmptyDataSourceParams, EmptyDataParams,
+  MyTrainingData, MyQuery, EmptyActual] {
+
+  /* override this to return Training Data only */
+  override
+  def readTraining(): MyTrainingData = {
+    val lines = Source.fromFile("data.csv").getLines()
+      .toList.map{ line =>
+        val data = line.split(",")
+        (data(0), data(1).toDouble)
+      }
+
+    new MyTrainingData(lines)
+  }
+}
+
+class MyAlgorithm extends LAlgorithm[EmptyAlgorithmParams, MyTrainingData,
+  MyModel, MyQuery, MyPrediction] {
+
+  override
+  def train(pd: MyTrainingData): MyModel = {
+
+    // Temporary data object... ten random pairs of user_id and parameter
+    val sampleUserId = 888
+    val sampleTargetItems = List( UserItemPair(Random.nextInt, Random.nextInt),
+                           UserItemPair(Random.nextInt, Random.nextInt),
+                           UserItemPair(Random.nextInt, Random.nextInt),
+                           UserItemPair(Random.nextInt, Random.nextInt),
+                           UserItemPair(Random.nextInt, Random.nextInt),
+                           UserItemPair(Random.nextInt, Random.nextInt),
+                           UserItemPair(Random.nextInt, Random.nextInt),
+                           UserItemPair(Random.nextInt, Random.nextInt),
+                           UserItemPair(Random.nextInt, Random.nextInt),
+                           UserItemPair(Random.nextInt, Random.nextInt) )
+
+    // Push randomization to prediction step, so that seed can be param of
+    // query.
+    // val shuffledTargetItems = Random.shuffle(sampleTargetItems)
+
+    new MyModel( Map(sampleUserId -> sampleTargetItems) )
+  }
+
+  override
+  def predict(model: MyModel, query: MyQuery): MyPrediction = {
+    Random.setSeed(query.randomSeed)
+    val randomRec = Random.shuffle(model.recommendations(query.userId))
+    new MyPrediction(randomRec)
+  }
+}
+
+// factory
+object MyEngineFactory extends IEngineFactory {
+  override
+  def apply() = {
+    /* SimpleEngine only requires one DataSouce and one Algorithm */
+    new SimpleEngine(
+      classOf[MyDataSource],
+      classOf[MyAlgorithm]
+    )
+  }
+}
