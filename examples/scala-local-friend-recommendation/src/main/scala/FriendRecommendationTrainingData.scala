f337906aa9d34efba66c0b409fbbfc6ae516cab1
Gary Zhang <garyzhang@Garys-MacBook-Pro.local>
2014-10-23 22:38:27 -0500

    Add an engine.json for keyword similarity, change training data file name to be unified, add a factory for keyword similarity.

--- examples/scala-local-friend-recommendation/src/main/scala/FriendRecommendationTrainingData.scala
+++ examples/scala-local-friend-recommendation/src/main/scala/FriendRecommendationTrainingData.scala
@@ -2,7 +2,9 @@ package io.prediction.examples.friendrecommendation
 
 import scala.collection.immutable.HashMap
 
-class KeywordSimilarityTrainingData (
+class FriendRecommendationTrainingData (
+  // Designed to fit random and keyword similarity methods now
+  // Will be updated to to fit more advanced algorithms when they are developed
   val userKeyword: HashMap[Int, HashMap[Int, Double]],
   val itemKeyword: HashMap[Int, HashMap[Int, Double]]
 ) extends Serializable
