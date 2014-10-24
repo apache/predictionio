8224671f5e8e3e36f8dd5b63a504378e0a311d08
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-10-24 13:27:25 -0700

    DataSource return mutable HashMap, need to change to immutable

--- examples/scala-local-friend-recommendation/src/main/scala/FriendRecommendationDataSource.scala
+++ examples/scala-local-friend-recommendation/src/main/scala/FriendRecommendationDataSource.scala
@@ -1,19 +1,48 @@
 package io.prediction.examples.friendrecommendation
 import io.prediction.controller._
 import scala.io.Source
+import collection.mutable.HashMap
 
 class FriendRecommendationDataSource extends LDataSource[EmptyDataSourceParams, EmptyDataParams, FriendRecommendationTrainingData, FriendRecommendationQuery, EmptyActual] {
   override
   def readTraining() : FriendRecommendationTrainingData = {
     val item = Source.fromFile("data/item.txt").getLines().map{ line =>
       val data = line.split("\\s+")
-      data(1).toInt
-    }.toList
+      val id = data(1).toInt
+      //key is value (Int) and value is its weight(Double)
+      var item_map = new HashMap[Int, Double]
+      for (keyword <- data(3).split(";")){
+        //Item keyword contain no weight
+        item_map += (keyword.toInt -> 1.0)
+      }
+      (id, item_map)
+    }
+    var item_keyword = new HashMap[Int, HashMap[Int, Double]]
+    for(element <- item){  
+      item_keyword += (element._1 -> element._2)
+    }
+
+    //Reading user profile
     val user = Source.fromFile("data/user_profile.txt").getLines().map{ line =>
       val data = line.split("\\s+")
-      data(1).toInt
-    }.toList
-    new FriendRecommendationTrainingData(user, item)
+      val id = data(1).toInt
+      //key is value (Int) and value is its weight(Double)
+      var user_map = new HashMap[Int, Double]
+      for (keyword <- data(5).split(";")){
+        //Item keyword contain no weight
+        user_map += (keyword.toInt -> 1.0)
+      }
+      (id, user_map)
+    }
+
+    var user_keyword = new HashMap[Int, HashMap[Int, Double]]
+    for(element <- user){
+      user_keyword += (element._1 -> element._2)
+       
+    }
+    //val user_kw_map = collection.immutable.HashMap() ++ user_keyword
+    //val item_kw_map = collection.immutable.HashMap() ++ item_keyword
+    new FriendRecommendationTrainingData(user_keyword, item_keyword)
 
   }
 }
--- examples/scala-local-friend-recommendation/src/main/scala/FriendRecommendationTrainingData.scala
+++ examples/scala-local-friend-recommendation/src/main/scala/FriendRecommendationTrainingData.scala
@@ -1,15 +1,16 @@
 package io.prediction.examples.friendrecommendation
 
 import scala.collection.immutable.HashMap
+//import scala.collection.mutable.HashMap
 
 class FriendRecommendationTrainingData (
   // Designed to fit random and keyword similarity methods now
   // Will be updated to to fit more advanced algorithms when they are developed
   
   //List of userid and itemid
-  val userId: List[Int],
-  val itemId: List[Int]
+  //val userId: List[Int],
+  //val itemId: List[Int]
   
-  //val userKeyword: HashMap[Int, HashMap[Int, Double]],
-  //val itemKeyword: HashMap[Int, HashMap[Int, Double]]
+  val userKeyword: HashMap[Int, HashMap[Int, Double]],
+  val itemKeyword: HashMap[Int, HashMap[Int, Double]]
 ) extends Serializable
