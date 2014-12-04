228a0aa8962921f903f6f05a16862154173e87e9
David Singer <david@ramaboo.com>
2014-12-03 18:54:00 -0800

    Enabling autoprefixer

--- docs/manual/config.rb
+++ docs/manual/config.rb
@@ -9,6 +9,7 @@ set :partials_dir,  'partials'
 activate :directory_indexes
 activate :gzip
 activate :syntax, line_numbers: true
+activate :autoprefixer
 
 # Markdown
 set :markdown_engine, :redcarpet
