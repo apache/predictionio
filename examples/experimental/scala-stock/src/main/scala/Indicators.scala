7c0487b563a5355cc4c86a7149135cba79bdf3e4
Gaurang Patel <gpatel@ucsd.edu>
2014-12-10 13:08:01 -0800

    Indicators.scala updated with documentation

--- examples/experimental/scala-stock/src/main/scala/Indicators.scala
+++ examples/experimental/scala-stock/src/main/scala/Indicators.scala
@@ -15,22 +15,47 @@ import math._
 
 import nak.regress.LinearRegression
 
-/*
- * Base class for indicators. All indicators should be defined as classes that 
- * extend this base class. See RSIIndicator as an example. These indicators can 
- * then be instantiated and passed into a StockStrategy class. Refer to 
- * tutorial for further explanation (found in the README.md file).
- */
+/**
+  * Base class for an indicator.
+  *
+  * All indicators should be defined as classes that extend
+  * this base class. See RSIIndicator as an example. These indicators can then
+  * be instantiated and passed into a StockStrategy class. Refer to tutorial 
+  * for further explanation (found in the README.md file).
+  */
 @SerialVersionUID(100L)
 abstract class BaseIndicator extends Serializable {
+	/** Calculates training series for a particular stock.
+    *
+	  * @param logPrice series of logarithm of all prices for a particular stock.
+	  *				   Logarithm values are recommended for more accurate results.
+	  * @return the training series of the stock
+	  */
   def getTraining(logPrice: Series[DateTime, Double]): Series[DateTime, Double]
+
+  /** Applies indicator on a window size of the value returned by 
+  	* getMinWindowSize() and returns the last value in the resulting series to
+  	* be used for prediction in RegressionStrategy.
+	  *
+		* @param logPrice series of logarithm of all prices for a particular stock
+		* @return the last value in the resulting series from the feature 
+		*          calculation
+		*/
   def getOne(input: Series[DateTime, Double]): Double
+
+  /** Returns window size to be used in getOne()
+	  *
+	  * @return the window size
+	  */
   def getMinWindowSize(): Int
 }
 
-/* Calculates RSI day averages
- * Input: period - differences in closing cost is calculated over
- */
+/** Indicator that implements a relative strength index formula
+  *
+	* @constructor create an instance of an RSIIndicator
+	* @param period number of days to use for each of the 14 periods
+	*         that are used in the RSI calculation
+  */
 class RSIIndicator(period: Int, RsiPeriod: Int = 14) extends BaseIndicator {
 
 	private def getRet(dailyReturn: Series[DateTime, Double]) =
@@ -75,7 +100,12 @@ class RSIIndicator(period: Int, RsiPeriod: Int = 14) extends BaseIndicator {
 	}
 }
 
-// Indicator that calcuates differences of closing prices
+/** Indicator that calcuate differences of closing prices
+  *
+	* @constructor create an instance of a ShiftsIndicator
+	* @param period number of days between any 2 closing prices to consider for 
+	*					calculating a return
+	*/
 class ShiftsIndicator(period: Int) extends BaseIndicator {
 
   private def getRet(logPrice: Series[DateTime, Double], frame: Int = period) =
