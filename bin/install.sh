dd1cb823171034bd2d989b63c5a317fd2802e207
David Singer <david@ramaboo.com>
2014-12-23 13:31:19 -0800

    Update and rename install-ubuntu.sh to install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -10,45 +10,81 @@
 #
 #
 
-INSTALL_DIR=/home/$USER
+OS=`uname`
+PIO_VERSION=0.8.4
+SPARK_VERSION=1.2.0
+ELASTICSEARCH_VERSION=1.3.3
+HBASE_VERSION=0.98.6
+
+if [ $OS = "Darwin" ]
+then
+  echo "Installing on Mac"
+  INSTALL_DIR=/Users/$USER
+  USER_PROFILE=/Users/$USER/.profile
+
+elif [ $OS = "Linux" ]
+then
+  echo "Installing on Linux"
+  INSTALL_DIR=/home/$USER
+  USER_PROFILE=/home/$USER/.profile
+else
+  echo "Platform not recognized! Aborting!"
+  exit 1
+fi
+
 TEMP_DIR=/tmp
-PIO_VERSION=0.8.5-SNAPSHOT
 PIO_DIR=$INSTALL_DIR/PredictionIO
 PIO_FILE=PredictionIO-$PIO_VERSION.tar.gz
 VENDORS_DIR=$PIO_DIR/vendors
-SPARK_DIR=$VENDORS_DIR/spark-1.2.0
-ELASTICSEARCH_DIR=$VENDORS_DIR/elasticsearch-1.3.2
-HBASE_DIR=$VENDORS_DIR/hbase-0.98.6
+SPARK_DIR=$VENDORS_DIR/spark-$SPARK_VERSION
+ELASTICSEARCH_DIR=$VENDORS_DIR/elasticsearch-$ELASTICSEARCH_VERSION
+HBASE_DIR=$VENDORS_DIR/hbase-$HBASE_VERSION
 ZOOKEEPER_DIR=$VENDORS_DIR/zookeeper
 
-echo "This script requires superuser access."
-echo "You will be prompted for your password by sudo."
-
 # Java
-echo "Starting Java install..."
-sudo apt-get install openjdk-7-jdk -y
+if [ $OS = "Darwin" ]
+  then
+  echo "Starting Java on Mac..."
 
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+  JAVA_VERSION=`echo "$(java -version 2>&1)" | grep "java version" | awk '{ print substr($3, 2, length($3)-2); }'`
+  JAVA_HOME=`/usr/libexec/java_home`
 
-echo "JAVA_HOME is now set to: $JAVA_HOME"
-echo "Java install done!"
+  echo "Your Java version is: $JAVA_VERSION"
+  echo "JAVA_HOME is now set to: $JAVA_HOME"
+  echo "Java done!"
+
+elif [ $OS = "Linux" ]
+  then
+  echo "This script requires superuser access."
+  echo "You will be prompted for your password by sudo."
+
+  # Java
+  echo "Starting Java install on Linux..."
+  sudo apt-get install openjdk-7-jdk -y
+
+  JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+
+  echo "JAVA_HOME is now set to: $JAVA_HOME"
+  echo "Java install done!"
+fi
+
 
 # PredictionIO
 echo "Starting PredictionIO setup in: $PIO_DIR"
 cd $TEMP_DIR
 if [ ! -e $PIO_FILE ]; then
   echo "Downloading PredictionIO..."
-  wget http://download.prediction.io/$PIO_FILE
+  curl -O http://download.prediction.io/$PIO_FILE
 fi
 tar zxf $PIO_FILE
 rm -rf $PIO_DIR
 mv PredictionIO-$PIO_VERSION $PIO_DIR
 
-chown -R $USER:$USER $PIO_DIR
+chown -R $USER $PIO_DIR
 
 echo "Updating ~/.profile to include: $PIO_DIR"
 PATH=$PATH:$PIO_DIR/bin
-echo "export PATH=\$PATH:$PIO_DIR/bin" >> /home/$USER/.profile
+echo "export PATH=\$PATH:$PIO_DIR/bin" >> $USER_PROFILE
 
 echo "PredictionIO setup done!"
 
@@ -56,28 +92,28 @@ mkdir $VENDORS_DIR
 
 # Spark
 echo "Starting Spark setup in: $SPARK_DIR"
-if [ ! -e spark-1.2.0-bin-hadoop2.4.tgz ]; then
+if [ ! -e spark-$SPARK_VERSION-bin-hadoop2.4.tgz ]; then
   echo "Downloading Spark..."
-  wget http://d3kbcqa49mib13.cloudfront.net/spark-1.2.0-bin-hadoop2.4.tgz
+  curl -O http://d3kbcqa49mib13.cloudfront.net/spark-$SPARK_VERSION-bin-hadoop2.4.tgz
 fi
-tar xf spark-1.2.0-bin-hadoop2.4.tgz
+tar xf spark-$SPARK_VERSION-bin-hadoop2.4.tgz
 rm -rf $SPARK_DIR
-mv spark-1.2.0-bin-hadoop2.4 $SPARK_DIR
+mv spark-$SPARK_VERSION-bin-hadoop2.4 $SPARK_DIR
 
 echo "Updating: $PIO_DIR/conf/pio-env.sh"
-sed -i "s|SPARK_HOME=/path_to_apache_spark|SPARK_HOME=$SPARK_DIR|g" $PIO_DIR/conf/pio-env.sh
+sed -i "" "s|SPARK_HOME=/path_to_apache_spark|SPARK_HOME=$SPARK_DIR|g" $PIO_DIR/conf/pio-env.sh
 
 echo "Spark setup done!"
 
 # Elasticsearch
 echo "Starting Elasticsearch setup in $ELASTICSEARCH_DIR"
-if [ ! -e elasticsearch-1.3.2.tar.gz ]; then
+if [ ! -e elasticsearch-$ELASTICSEARCH_VERSION.tar.gz ]; then
   echo "Downloading Elasticsearch..."
-  wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.2.tar.gz
+  curl -O https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
 fi
-tar zxf elasticsearch-1.3.2.tar.gz
+tar zxf elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
 rm -rf $ELASTICSEARCH_DIR
-mv elasticsearch-1.3.2 $ELASTICSEARCH_DIR
+mv elasticsearch-$ELASTICSEARCH_VERSION $ELASTICSEARCH_DIR
 
 
 echo "Updating: $ELASTICSEARCH_DIR/config/elasticsearch.yml"
@@ -87,13 +123,13 @@ echo "Elasticsearch setup done!"
 
 # HBase
 echo "Starting HBase setup in $HBASE_DIR"
-if [ ! -e hbase-0.98.6-hadoop2-bin.tar.gz ]; then
+if [ ! -e hbase-$HBASE_VERSION-hadoop2-bin.tar.gz ]; then
   echo "Downloading HBase..."
-  wget http://archive.apache.org/dist/hbase/hbase-0.98.6/hbase-0.98.6-hadoop2-bin.tar.gz
+  curl -O http://archive.apache.org/dist/hbase/hbase-$HBASE_VERSION/hbase-$HBASE_VERSION-hadoop2-bin.tar.gz
 fi
-tar zxf hbase-0.98.6-hadoop2-bin.tar.gz
+tar zxf hbase-$HBASE_VERSION-hadoop2-bin.tar.gz
 rm -rf $HBASE_DIR
-mv hbase-0.98.6-hadoop2 $HBASE_DIR
+mv hbase-$HBASE_VERSION-hadoop2 $HBASE_DIR
 
 echo "Creating default site in: $HBASE_DIR/conf/hbase-site.xml"
 cat <<EOT > $HBASE_DIR/conf/hbase-site.xml
@@ -110,13 +146,13 @@ cat <<EOT > $HBASE_DIR/conf/hbase-site.xml
 EOT
 
 echo "Updating: $HBASE_DIR/conf/hbase-env.sh to include $JAVA_HOME"
-sed -i "s|# export JAVA_HOME=/usr/java/jdk1.7.0/|export JAVA_HOME=$JAVA_HOME|" $HBASE_DIR/conf/hbase-env.sh
+sed -i "" "s|# export JAVA_HOME=/usr/java/jdk1.6.0/|export JAVA_HOME=$JAVA_HOME|" $HBASE_DIR/conf/hbase-env.sh
 
 echo "HBase setup done!"
 
 echo "Updating permissions on: $VENDORS_DIR"
 
-chown -R $USER:$USER $VENDORS_DIR
+chown -R $USER $VENDORS_DIR
 
 echo "Starting Elasticserach and HBase!"
 
