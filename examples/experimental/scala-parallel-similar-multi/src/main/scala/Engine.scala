f6b84e6dd84ee3abb3b1b3ae59d50dfdcf328e0f
Kenneth Chan <kenneth@prediction.io>
2015-01-14 19:08:54 -0800

    PDIO-480 Example of using multiple algorithms to handle different user to item events

---
+++ examples/experimental/scala-parallel-similar-multi/src/main/scala/Engine.scala
@@ -0,0 +1,34 @@
+package org.template.similar
+
+import io.prediction.controller.IEngineFactory
+import io.prediction.controller.Engine
+
+case class Query(
+  val items: List[String],
+  val num: Int,
+  val categories: Option[Set[String]],
+  val whiteList: Option[Set[String]],
+  val blackList: Option[Set[String]]
+) extends Serializable
+
+case class PredictedResult(
+  val itemScores: Array[ItemScore]
+) extends Serializable {
+  override def toString = itemScores.mkString(",")
+}
+
+case class ItemScore(
+  item: String,
+  score: Double
+) extends Serializable
+
+object SimilarityEngine extends IEngineFactory {
+  def apply() = {
+    new Engine(
+      classOf[DataSource],
+      classOf[Preparator],
+      Map("als" -> classOf[ALSAlgorithm],
+        "like" -> classOf[LikeAlgorithm]),
+      classOf[Serving])
+  }
+}
---
+++ examples/experimental/scala-parallel-similar-multi/src/main/scala/Serving.scala
@@ -0,0 +1,42 @@
+package org.template.similar
+
+import io.prediction.controller.LServing
+
+import breeze.stats.mean
+import breeze.stats.meanAndVariance
+import breeze.stats.MeanAndVariance
+
+class Serving
+  extends LServing[Query, PredictedResult] {
+
+  override
+  def serve(query: Query,
+    predictedResults: Seq[PredictedResult]): PredictedResult = {
+
+    // MODFIED
+    // Standardize the score before combine
+    val mvList: Seq[MeanAndVariance] = predictedResults.map { pr =>
+      meanAndVariance(pr.itemScores.map(_.score))
+    }
+
+    val standardResults: Seq[Array[ItemScore]] = predictedResults.zipWithIndex
+      .map { case (pr, i) =>
+        pr.itemScores.map { is =>
+          // standardize score (z-score)
+          val score = (is.score - mvList(i).mean) / mvList(i).stdDev
+          ItemScore(is.item, score)
+        }
+      }
+
+    // sum the standardized score if same item
+    val combined = standardResults.flatten // Array of ItemScore
+      .groupBy(_.item) // groupBy item id
+      .mapValues(itemScores => itemScores.map(_.score).reduce(_ + _))
+      .toArray // array of (item id, score)
+      .sortBy(_._2)(Ordering.Double.reverse)
+      .take(query.num)
+      .map { case (k,v) => ItemScore(k, v) }
+
+    new PredictedResult(combined)
+  }
+}
