5949a80b48f5b9ed59ccd37c3e0b7589b2685bdf
Kenneth Chan <kenneth@prediction.io>
2015-01-22 23:34:10 -0800

    PDIO-487 PDIO-509 Fix NaN and empty iterator error in Similar product template - filter items with score > 0 - check if productFeatures contain query items

--- templates/scala-parallel-similarproduct/src/main/scala/ALSAlgorithm.scala
+++ templates/scala-parallel-similarproduct/src/main/scala/ALSAlgorithm.scala
@@ -150,9 +150,11 @@ class ALSAlgorithm(val ap: ALSAlgorithmParams)
 
     val queryFeatures: Vector[Array[Double]] = queryList.toVector.par
       .map { item =>
-        val qf: Array[Double] = model.productFeatures.lookup(item).head
+        // productFeatures may not contain the requested item
+        val qf: Option[Array[Double]] = model.productFeatures
+          .lookup(item).headOption
         qf
-      }.seq
+      }.seq.flatten
 
     val whiteList: Option[Set[Int]] = query.whiteList.map( set =>
       set.map(model.itemStringIntMap.get(_)).flatten
@@ -164,7 +166,7 @@ class ALSAlgorithm(val ap: ALSAlgorithmParams)
     val ord = Ordering.by[(Int, Double), Double](_._2).reverse
 
     val indexScores: Array[(Int, Double)] = if (queryFeatures.isEmpty) {
-      logger.info(s"No valid items in ${query.items}.")
+      logger.info(s"No productFeatures vector for query items ${query.items}.")
       Array[(Int, Double)]()
     } else {
       model.productFeatures
@@ -173,6 +175,7 @@ class ALSAlgorithm(val ap: ALSAlgorithmParams)
             cosine(qf, f)
           }.reduce(_ + _)
         }
+        .filter(_._2 > 0) // keep items with score > 0
         .collect()
     }
 
