231e6e50deb540f99c46a9e71214a72ec6fdc974
Rolando <rolando@smartbeemo.com>
2015-01-26 16:35:04 -0500

    updated with admin api branch changes

--- data/src/main/scala/storage/CommandClient.scala
+++ data/src/main/scala/storage/CommandClient.scala
@@ -20,10 +20,35 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext
 
-case class AppNewRequest()
-case class AppNewResponse()
+abstract class BaseResponse(s: Int, m: String) {
+  def getStatus = s
+  def getMessage = m
+}
 
-case class AppListResponse()
+case class GeneralResponse(
+                            status: Int = 0,
+                            message: String = "") extends BaseResponse(status,message)
+
+case class AppRequest(
+                       id: Int = 0,
+                       name: String = "",
+                       description: String = ""
+                       )
+
+case class AppResponse(
+                        status: Int = 0,
+                        message: String = "",
+                        id: Int = 0,
+                        name: String = "",
+                        keys: Seq[AccessKey]
+                        ) extends BaseResponse(status,message)
+
+
+case class AppListResponse(
+                        status: Int = 0,
+                        message: String = "",
+                        apps: Seq[AppResponse]
+                            ) extends BaseResponse(status,message)
 
 // see Console.scala for reference
 
@@ -34,9 +59,52 @@ class CommandClient (
 ) {
 
   // see def appNew() in Console.scala
-  def futureAppNew(req: AppNewRequest)
-    (implicit ec: ExecutionContext): Future[AppNewResponse] = Future {
-    AppNewResponse()
+  def futureAppNew(req: AppRequest)
+    (implicit ec: ExecutionContext): Future[BaseResponse] = Future {
+    val apps = Storage.getMetaDataApps
+    val response = apps.getByName(req.name) map { app =>
+      GeneralResponse(0, s"App ${req.name} already exists. Aborting.")
+    } getOrElse {
+      apps.get(req.id) map {
+        app2 =>
+          GeneralResponse(0, s"App ID ${app2.id} already exists and maps to the app '${app2.name}'. " +
+            "Aborting.")
+      } getOrElse {
+        val appid = apps.insert(App(
+          id = Option(req.id).getOrElse(0),
+          name = req.name,
+          description = Option(req.description)))
+        appid map { id =>
+          val events = Storage.getLEvents()
+          val dbInit = events.init(id)
+          val r = if (dbInit) {
+
+            val accessKeys = Storage.getMetaDataAccessKeys
+            val accessKey = AccessKey(
+              key = "",
+              appid = id,
+              events = Seq())
+
+            val accessKey2 = accessKeys.insert(AccessKey(
+              key = "",
+              appid = id,
+              events = Seq()))
+
+            accessKey2 map { k =>
+              new AppResponse(1,"App created successfuly.",id, req.name, Seq[AccessKey](accessKey))
+            } getOrElse {
+              GeneralResponse(0, s"Unable to create new access key.")
+            }
+          } else {
+            GeneralResponse(0, s"Unable to initialize Event Store for this app ID: ${id}.")
+          }
+          r
+        } getOrElse {
+          GeneralResponse(0, s"Unable to create new app.")
+        }
+      }
+    }
+    response
   }
 
   // see def appList() in Console.scala
