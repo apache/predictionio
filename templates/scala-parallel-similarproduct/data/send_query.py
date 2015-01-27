618f777a28f6e7499ed4001fd1c2c177b9a8687f
Donald Szeto <donald@prediction.io>
2015-01-27 08:22:30 -0800

    Emigrated from core to external repos

--- templates/scala-parallel-similarproduct/data/send_query.py
+++
@@ -1,7 +0,0 @@
-"""
-Send sample query to prediction engine
-"""
-
-import predictionio
-engine_client = predictionio.EngineClient(url="http://localhost:8000")
-print engine_client.send_query({"items": ["i1", "i3"], "num": 4})
