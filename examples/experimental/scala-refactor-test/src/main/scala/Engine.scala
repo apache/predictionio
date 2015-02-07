753a6605022f82baee43165cbb5ea17c52509ae7
Justin Yip <yipjustin@prediction.io>
2015-02-07 02:08:26 -0800

    refactor code test init checkin

---
+++ examples/experimental/scala-refactor-test/src/main/scala/Engine.scala
@@ -0,0 +1,18 @@
+package pio.refactor
+
+import io.prediction.controller.IEngineFactory
+import io.prediction.controller.Engine
+
+case class Query(q: String) extends Serializable
+
+case class PredictedResult(p: String) extends Serializable
+
+object VanillaEngine extends IEngineFactory {
+  def apply() = {
+    new Engine(
+      classOf[DataSource],
+      classOf[Preparator],
+      Map("algo" -> classOf[Algorithm]),
+      classOf[Serving])
+  }
+}
