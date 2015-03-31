d9af40c60682f52a987095f8651f1618abebe5db
Jonathan Glidden <jhglidden@gmail.com>
2015-03-31 13:43:48 -0700

    remove unnecessary map partition unzip

--- e2/src/test/scala/io/prediction/e2/evaluation/CrossValidationTest.scala
+++ e2/src/test/scala/io/prediction/e2/evaluation/CrossValidationTest.scala
@@ -91,9 +91,8 @@ with SharedSparkContext{
     val diffs = splits.map { case (k, folds) =>
       folds.map(fold => fold._3.count() - dataCount / k)
     }
-    diffs.flatMap(fold => if (fold.exists(_ > 1)) Some(true) else None) should be('empty)
-    val (zeros, ones) = diffs.map(diffSequence => diffSequence.partition(_ == 0)).unzip
-    ones.map(_.length) should be(evalKs.map(k => dataCount % k))
+    diffs.flatMap(folds => if (folds.exists(_ > 1)) Some(true) else None) should be('empty)
+    diffs.map(folds => folds.sum) should be(evalKs.map(k => dataCount % k))
   }
 
 
