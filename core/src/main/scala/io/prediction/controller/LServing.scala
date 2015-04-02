502bab5e0065fdaf4364af528d96f4863baf250c
Donald Szeto <donald@prediction.io>
2015-04-01 18:21:38 -0700

    PDIO-604 Improve Scaladoc for Serving

--- core/src/main/scala/io/prediction/controller/LServing.scala
+++ core/src/main/scala/io/prediction/controller/LServing.scala
@@ -13,10 +13,27 @@
   * limitations under the License.
   */
 
-package io.prediction.core
+package io.prediction.controller
 
-abstract class BaseServing[Q, P]
-  extends AbstractDoer {
+import io.prediction.core.BaseServing
+
+/** Base class of serving. 
+  *
+  * @tparam Q Input query class.
+  * @tparam P Output prediction class.
+  * @group Serving
+  */
+abstract class LServing[Q, P] extends BaseServing[Q, P] {
   private[prediction]
-  def serveBase(q: Q, ps: Seq[P]): P
+  def serveBase(q: Q, ps: Seq[P]): P = {
+    serve(q, ps)
+  }
+
+  /** Implement this method to combine multiple algorithms' predictions to
+    * produce a single final prediction.
+    *
+    * @param query Input query.
+    * @param predictions A list of algorithms' predictions.
+    */
+  def serve(query: Q, predictions: Seq[P]): P
 }
