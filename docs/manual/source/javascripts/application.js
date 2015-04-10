ec06335d6a86c9dc7b12f545da8170d53c4a424a
David Singer <david@ramaboo.com>
2015-04-10 14:40:05 -0700

    First pass

--- docs/manual/source/javascripts/application.js
+++ docs/manual/source/javascripts/application.js
@@ -3,13 +3,39 @@
 //= require 'jcarousel'
 
 $(document).ready(function() {
+  function navExpand(link) {
+    link.removeClass('expandible').addClass('collapsible');
+    link.children('i').removeClass('fa-caret-right').addClass('fa-caret-down');
+    link.next('ul').show();
+  }
+  
+  function navCollapse(link) {
+    link.removeClass('collapsible').addClass('expandible');
+    link.children('i').removeClass('fa-caret-down').addClass('fa-caret-right');
+    link.next('ul').hide();
+  }
+  
   // Main Navigation
-  $('#nav-main > ul > li > a').on('click', function(event) {
-    event.preventDefault();
-    $(this).next().toggle();
+  $('#nav-main a').on('click', function(event) {
+    var $this = $(this);
+    
+    if ($this.hasClass('expandible')) {
+      navExpand($this);
+      event.preventDefault();
+    } else if ($this.hasClass('collapsible')) {
+      navCollapse($this);
+      event.preventDefault();
+    }
   });
-
-  $('#nav-main .active').parent().parent().show();
+  
+  $('#nav-main .active').parentsUntil('#nav-main').each(function() {
+    $(this).children('.expandible').each(function() {
+      var $this = $(this);
+      if (!$this.hasClass('active')) {
+        navExpand($this);
+      }
+    });
+  });
 
   $('#content').on('click', function(event) {
     $('body').removeClass('active-navigation active-complementary')
