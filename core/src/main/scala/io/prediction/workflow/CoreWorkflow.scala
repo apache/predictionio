482208053f82a35a8759a466b94c44853d9d794a
Justin Yip <yipjustin@prediction.io>
2015-04-19 14:14:42 -0700

    Skip save option in evaluator result

--- core/src/main/scala/io/prediction/workflow/CoreWorkflow.scala
+++ core/src/main/scala/io/prediction/workflow/CoreWorkflow.scala
@@ -124,18 +124,21 @@ object CoreWorkflow {
       engineParamsList,
       evaluator,
       params)
-    val evaluatedEvaluationInstance = evaluationInstance.copy(
-      status = "EVALCOMPLETED",
-      id = evaluationInstanceId,
-      endTime = DateTime.now,
-      evaluatorResults = evaluatorResult.toOneLiner,
-      evaluatorResultsHTML = evaluatorResult.toHTML,
-      evaluatorResultsJSON = evaluatorResult.toJSON
-    )
 
-    logger.info(s"Updating evaluation instance with result: $evaluatorResult")
-
-    evaluationInstances.update(evaluatedEvaluationInstance)
+    if (evaluatorResult.noSave) {
+      logger.info(s"This evaluation result is not inserted into database")
+    } else {
+      val evaluatedEvaluationInstance = evaluationInstance.copy(
+        status = "EVALCOMPLETED",
+        id = evaluationInstanceId,
+        endTime = DateTime.now,
+        evaluatorResults = evaluatorResult.toOneLiner,
+        evaluatorResultsHTML = evaluatorResult.toHTML,
+        evaluatorResultsJSON = evaluatorResult.toJSON
+      )
+      logger.info(s"Updating evaluation instance with result: $evaluatorResult")
+      evaluationInstances.update(evaluatedEvaluationInstance)
+    }
 
     logger.debug("Stop SparkContext")
 
