4833688243d2ad3fd9da7ca3b2f6810f003ca931
Marcin Ziemiński <zieminm@gmail.com>
2016-07-26 14:24:07 -0700

    Provide base class for all the test cases

---
+++ integration.py
@@ -0,0 +1,24 @@
+import unittest
+
+class TestContext:
+    def __init__(self, engine_directory, data_directory, es_ip, es_port):
+        self.engine_directory = engine_directory
+        self.data_directory = data_directory
+        self.es_ip = es_ip
+        self.es_port = es_port
+
+# The base class for all the tests cases requiring eventserver
+class BaseTestCase(unittest.TestCase):
+
+    def __init__(self, methodName='runTest', test_context):
+        super(ParametrizedTestCase, self).__init__(methodName)
+        self.test_context = test_context
+
+    @classmethod
+    def setUpClass(cls):
+        # start the eventserver
+        pass
+
+    def tearDownClass(cls):
+        # stop the eventserver
+        pass
