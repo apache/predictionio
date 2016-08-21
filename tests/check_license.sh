19a8739c111825741273ea0557abf0c4a901d0a8
Chan Lee <chanlee514@gmail.com>
2016-08-21 12:26:26 -0700

    [PIO-26] Integrate Apache RAT for license checking
    Closes apache/incubator-predictionio#279
    
    Squashed commit of the following:
    
    commit 53c408007dfb93ba4526feec1d71c0e6579674ec
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Fri Aug 19 15:25:31 2016 -0700
    
        Fix license header location for html files
    
        All comments should come after @import and <!DOCTYPE html> declaration
    
    commit 4a86904dae8abf2c0b2e75f96cbdb33801f3bf4e
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Fri Aug 19 13:34:34 2016 -0700
    
        Configure RAT Apache license checker to run on travis
    
    commit 0beb4be3f32021902c0904b69207a2f9e6dcce4b
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Fri Aug 19 10:47:53 2016 -0700
    
        Add apache license to source files in docs/
    
    commit 1ab32d1cc9174cafc1ea8c7c0ca21221255b452d
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Fri Aug 19 10:10:19 2016 -0700
    
        Add apache license to engine source files used for integration testing
    
    commit d3cb62f73393ce7ca0bc4317e2b80d4411dcb052
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Fri Aug 19 10:07:50 2016 -0700
    
        Add apache license to more build.sbt files in examples/
    
    commit 0d18dda1025299e4ef8b30dfa8068863c35fdf07
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Thu Aug 18 17:41:16 2016 -0700
    
        Add apache license for empty __init__.py files
    
    commit 473df0afa775eea8642707365fc3b5a32d3f5b16
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Thu Aug 18 17:39:20 2016 -0700
    
        Add apache license for integration test files
    
    commit 7a2d97a49c41972a3d3efdf95b50ee4ff09e0dd7
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Thu Aug 18 17:34:15 2016 -0700
    
        Add apache license to some html files
    
    commit 611a3eff5b8ab65644c3d1ee6fdaddbbaf7ef8a5
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Thu Aug 18 17:11:15 2016 -0700
    
        Add apache license for travis files
    
    commit a422c44d7ce315ce73a1d2ec3da4b0cd790e2bea
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Thu Aug 18 17:05:40 2016 -0700
    
        Change indentation for bin/semver.sh
    
    commit b0a3d3c3310c1ae6c8b1f67d52f340e802f025ce
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Thu Aug 18 16:57:46 2016 -0700
    
        Put file description before apache license
    
    commit 1e6399f59d4fb2d122be0e5fa5cdd7c1c2fda04e
    Author: Chan Lee <chanlee514@gmail.com>
    Date:   Thu Aug 18 16:47:05 2016 -0700
    
        Add apache license for build.sbt files in examples/

---
+++ tests/check_license.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Go to PredictionIO directory
+FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
+mkdir -p ${FWDIR}/lib
+cd ${FWDIR}/lib
+
+# Download RAT jar in lib/
+RAT_VERSION=0.11
+RAT_JAR="${FWDIR}/lib/apache-rat-${RAT_VERSION}.jar"
+URL="http://repo1.maven.org/maven2/org/apache/rat/apache-rat/${RAT_VERSION}/apache-rat-${RAT_VERSION}.jar"
+if [ ! -f "$RAT_JAR" ]; then
+  if [ $(command -v curl) ]; then
+    curl -OL --silent "${URL}"
+  elif [ $(command -v wget) ]; then
+    wget --quiet ${URL}
+  fi
+fi
+if [ ! -f "$RAT_JAR" ]; then
+  echo "${RAT_JAR} download failed. Please install rat manually.\n"
+  exit 1
+fi
+
+# Run RAT testing
+TEST_DIR="${FWDIR}/tests"
+REPORT_DIR="${FWDIR}/test-reports"
+mkdir ${REPORT_DIR}
+java -jar ${RAT_JAR} -E ${TEST_DIR}/.rat-excludes -d ${FWDIR} > ${REPORT_DIR}/rat-results.txt
+if [ $? -ne 0 ]; then
+ echo "RAT exited abnormally"
+ exit 1
+fi
+
+# Print results
+ERRORS="$(cat ${REPORT_DIR}/rat-results.txt | grep -e "??")"
+if test ! -z "$ERRORS"; then
+  echo "Could not find Apache license headers in the following files:"
+  echo "$ERRORS"
+  exit 1
+else
+  echo -e "RAT checks passed."
+fi
